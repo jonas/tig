jonas/tig

commit 11359638c5dfc91b4b14a003a0519b2a5bc7f89d

status: improve title and report messages

tig.c
@@ -1368,7 +1368,7 @@ update_view_title(struct view *view)
 
 	assert(view_is_displayed(view));
 
-	if (view->lines || view->pipe) {
+	if (!VIEW(REQ_VIEW_STATUS) && (view->lines || view->pipe)) {
 		unsigned int view_lines = view->offset + view->height;
 		unsigned int lines = view->lines
 				   ? MIN(view_lines, view->lines) * 100 / view->lines
@@ -3151,15 +3151,16 @@ status_update(struct view *view)
 		assert(view->lines);
 
 		if (!line->data) {
-			if (line[1].type == LINE_STAT_NONE) {
-				report("Nothing to update");
-				return;
-			}
-
 			while (++line < view->line + view->lines && line->data) {
 				if (!status_update_file(view, line->data, line->type))
 					report("Failed to update file status");
 			}
+
+			if (!line[-1].data) {
+				report("Nothing to update");
+				return;
+			}
+
 
 		} else if (!status_update_file(view, line->data, line->type)) {
 			report("Failed to update file status");
@@ -3174,29 +3175,38 @@ status_update(struct view *view)
 static void
 status_select(struct view *view, struct line *line)
 {
+	struct status *status = line->data;
+	char file[SIZEOF_STR] = "all files";
 	char *text;
+
+	if (status && !string_format(file, "'%s'", status->name))
+		return;
+
+	if (!status && line[1].type == LINE_STAT_NONE)
+		line++;
 
 	switch (line->type) {
 	case LINE_STAT_STAGED:
-		text = "Press %s to unstage file for commit";
+		text = "Press %s to unstage %s for commit";
 		break;
 
 	case LINE_STAT_UNSTAGED:
-		text = "Press %s to stage file for commit  ";
+		text = "Press %s to stage %s for commit";
 		break;
 
 	case LINE_STAT_UNTRACKED:
-		text = "Press %s to stage file for addition";
+		text = "Press %s to stage %s for addition";
 		break;
 
 	case LINE_STAT_NONE:
-		return;
+		text = "Nothing to update";
+		break;
 
 	default:
 		die("w00t");
 	}
 
-	string_format(view->ref, text, get_key(REQ_STATUS_UPDATE));
+	string_format(view->ref, text, get_key(REQ_STATUS_UPDATE), file);
 }
 
 static bool

commit 249016a60b0dd8f8db59202b963e53c8ada53095

Fix updating of the view title to show the current position in the view
It regressed in 11359638c5dfc91b4b14a003a0519b2a5bc7f89d.

tig.c
@@ -1371,7 +1371,7 @@ update_view_title(struct view *view)
 
 	assert(view_is_displayed(view));
 
-	if (!VIEW(REQ_VIEW_STATUS) && (view->lines || view->pipe)) {
+	if (view != VIEW(REQ_VIEW_STATUS) && (view->lines || view->pipe)) {
 		unsigned int view_lines = view->offset + view->height;
 		unsigned int lines = view->lines
 				   ? MIN(view_lines, view->lines) * 100 / view->lines