jonas/tig

commit e00ea5d38044a525bfccf8bacd8384788d7ca97e

Blame: do to not reload the diff view for same commits

tig.c
@@ -3779,6 +3779,10 @@ blame_request(struct view *view, enum request request, struct line *line)
 			break;
 		}
 
+		if (view_is_displayed(VIEW(REQ_VIEW_DIFF)) &&
+		    !strcmp(blame->commit->id, VIEW(REQ_VIEW_DIFF)->ref))
+			break;
+
 		if (!strcmp(blame->commit->id, NULL_ID)) {
 			char path[SIZEOF_STR];
 

commit 6791c11c3bef57c9018a8e9bb36ce2ef7b0377fe

Blame view: fix reloading of the diff view for changes not yet committed
The regression was introduced in e00ea5d (Blame: do to not reload the
diff view for same commits).

tig.c
@@ -4184,6 +4184,8 @@ blame_request(struct view *view, enum request request, struct line *line)
 		}
 
 		open_view(view, REQ_VIEW_DIFF, flags);
+		if (VIEW(REQ_VIEW_DIFF)->pipe && !strcmp(blame->commit->id, NULL_ID))
+			string_copy_rev(VIEW(REQ_VIEW_DIFF)->ref, NULL_ID);
 		break;
 
 	default: