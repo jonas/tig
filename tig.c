jonas/tig

commit 49dc4030694e22b7f141bfb13565783985c48b77

Do not terminate the update when switching back to a loading view
Move call to end_update() to begin_update() so checks for view content
is honored in the case where no reloading is necessary.

tig.c
@@ -2622,6 +2622,9 @@ prepare_update_file(struct view *view, const char *name)
 static bool
 begin_update(struct view *view, bool refresh)
 {
+	if (view->pipe)
+		end_update(view, TRUE);
+
 	if (refresh) {
 		if (!start_io(&view->io))
 			return FALSE;
@@ -2854,9 +2857,6 @@ open_view(struct view *prev, enum request request, enum open_flags flags)
 	if (nviews != displayed_views() ||
 	    (nviews == 1 && base_view != display[0]))
 		resize_display();
-
-	if (view->pipe)
-		end_update(view, TRUE);
 
 	if (view->ops->open) {
 		if (!view->ops->open(view)) {

commit 0879fa98ad7fe921274fb3bdf91b59e4ba60ef56

Also call end_update for views with custom open function
Broken by 49dc4030694e22b7f141bfb13565783985c48b77

tig.c
@@ -2925,6 +2925,8 @@ open_view(struct view *prev, enum request request, enum open_flags flags)
 		resize_display();
 
 	if (view->ops->open) {
+		if (view->pipe)
+			end_update(view, TRUE);
 		if (!view->ops->open(view)) {
 			report("Failed to load %s view", view->name);
 			return;