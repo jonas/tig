jonas/tig

commit 33e10c2599e16ff6690e7b0bbdd7a95e7f97c886

Move nodelay logic to the get_input read loop

tig.c
@@ -68,7 +68,6 @@
 static void __NORETURN die(const char *err, ...);
 static void warn(const char *msg, ...);
 static void report(const char *msg, ...);
-static void set_nonblocking_input(bool loading);
 static size_t utf8_length(const char **start, size_t skip, int *width, size_t max_width, int *trimmed, bool reserve, int tab_size);
 static inline unsigned char utf8_char_length(const char *string, const char *end);
 
@@ -3041,7 +3040,6 @@ end_update(struct view *view, bool force)
 	while (!view->ops->read(view, NULL))
 		if (!force)
 			return;
-	set_nonblocking_input(FALSE);
 	if (force)
 		kill_io(view->pipe);
 	done_io(view->pipe);
@@ -3051,7 +3049,6 @@ end_update(struct view *view, bool force)
 static void
 setup_update(struct view *view, const char *vid)
 {
-	set_nonblocking_input(TRUE);
 	reset_view(view);
 	string_copy_rev(view->vid, vid);
 	view->pipe = &view->io;
@@ -6972,17 +6969,6 @@ report(const char *msg, ...)
 	update_view_title(view);
 }
 
-/* Controls when nodelay should be in effect when polling user input. */
-static void
-set_nonblocking_input(bool loading)
-{
-	static unsigned int loading_views;
-
-	if ((loading == FALSE && loading_views-- == 1) ||
-	    (loading == TRUE  && loading_views++ == 0))
-		nodelay(status_win, loading);
-}
-
 static void
 init_display(void)
 {
@@ -7050,6 +7036,7 @@ get_input(int prompt_position)
 {
 	struct view *view;
 	int i, key, cursor_y, cursor_x;
+	bool loading = FALSE;
 
 	if (prompt_position)
 		input_mode = TRUE;
@@ -7061,6 +7048,8 @@ get_input(int prompt_position)
 			    use_scroll_redrawwin)
 				redrawwin(view->win);
 			view->has_scrolled = FALSE;
+			if (view->pipe)
+				loading = TRUE;
 		}
 
 		/* Update the cursor position. */
@@ -7077,6 +7066,7 @@ get_input(int prompt_position)
 
 		/* Refresh, accept single keystroke of input */
 		doupdate();
+		nodelay(status_win, loading);
 		key = wgetch(status_win);
 
 		/* wgetch() with nodelay() enabled returns ERR when

commit c2c2839e98eb79c86012845c063680c3b8c8ecf0

Fix clearing of the loading flag in the input select loop
The bug was introduced in 33e10c2599e16ff6690e7b0bbdd7a95e7f97c886.

tig.c
@@ -7086,12 +7086,13 @@ get_input(int prompt_position)
 {
 	struct view *view;
 	int i, key, cursor_y, cursor_x;
-	bool loading = FALSE;
 
 	if (prompt_position)
 		input_mode = TRUE;
 
 	while (TRUE) {
+		bool loading = FALSE;
+
 		foreach_view (view, i) {
 			update_view(view);
 			if (view_is_displayed(view) && view->has_scrolled &&