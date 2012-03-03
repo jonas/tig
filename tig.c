jonas/tig

commit dbb3663c47b59fcca040fbac14fd6e20b734232d

Only reload the view after changing diff options for diff-like views
... as originally intended.

tig.c
@@ -1619,6 +1619,7 @@ enum view_flag {
 	VIEW_OPEN_DIFF		= 1 << 4,
 	VIEW_NO_REF		= 1 << 5,
 	VIEW_NO_GIT_DIR		= 1 << 6,
+	VIEW_DIFF_LIKE		= 1 << 7,
 };
 
 #define view_has_flags(view, flag)	((view)->ops->flags & (flag))
@@ -3272,8 +3273,8 @@ view_driver(struct view *view, enum request request)
 	case REQ_TOGGLE_REV_GRAPH:
 	case REQ_TOGGLE_REFS:
 	case REQ_TOGGLE_IGNORE_SPACE:
-		if (toggle_option(request))
-			reload_view(view);
+		if (toggle_option(request) && view_has_flags(view, VIEW_DIFF_LIKE))
+			refresh_view(view);
 		break;
 
 	case REQ_TOGGLE_SORT_FIELD:
@@ -4101,7 +4102,7 @@ diff_select(struct view *view, struct line *line)
 
 static struct view_ops diff_ops = {
 	"line",
-	VIEW_ADD_DESCRIBE_REF | VIEW_ADD_PAGER_REFS,
+	VIEW_DIFF_LIKE | VIEW_ADD_DESCRIBE_REF | VIEW_ADD_PAGER_REFS,
 	sizeof(struct diff_state),
 	diff_open,
 	diff_read,
@@ -6458,7 +6459,7 @@ stage_read(struct view *view, char *data)
 
 static struct view_ops stage_ops = {
 	"line",
-	VIEW_NO_FLAGS,
+	VIEW_DIFF_LIKE,
 	sizeof(struct stage_state),
 	stage_open,
 	stage_read,

commit aa539eafbc2c1a18b41f9aca284410e4b3a7a70d

Reload the view on a diff-like option change
view->argv needs to be updated for the diff view, so refreshing with the
same argv isn't enough.  Refactoring in dbb3663c introduced the bug.

Toggling whitespace still worked for the stage view since stage_open
calls argv_copy before begin_update; diff_open instead relies on
begin_update calling format_argv when appropriate.

tig.c
@@ -3274,7 +3274,7 @@ view_driver(struct view *view, enum request request)
 	case REQ_TOGGLE_REFS:
 	case REQ_TOGGLE_IGNORE_SPACE:
 		if (toggle_option(request) && view_has_flags(view, VIEW_DIFF_LIKE))
-			refresh_view(view);
+			reload_view(view);
 		break;
 
 	case REQ_TOGGLE_SORT_FIELD: