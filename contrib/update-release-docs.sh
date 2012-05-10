5beeb74c980206cb5172f569340f9312bc9bf02d
Jonas Fonseca <fonseca@diku.dk>
2012-05-10 09:24:37 -0400

    Add script to prepare documentation for http://jonas.nitro.dk/tig

---
+++ contrib/update-release-docs.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# Prepare documentation for http://jonas.nitro.dk/tig/
+#
+# Copyright (c) 2009-2012 Jonas Fonseca <fonseca@diku.dk>
+
+LATEST="$(git describe | sed 's/-[0-9]*-g[0-9a-f]*//')"
+DOCDIR="release-docs/"
+
+TRACKER="
+<script type=\"text/javascript\">
+var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
+document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
+</script>
+<script type=\"text/javascript\">
+try {
+var pageTracker = _gat._getTracker(\"UA-7492166-1\");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+"
+
+mkdir -p "$DOCDIR/releases"
+
+for file in tig.1.html tigrc.5.html manual.html README.html NEWS.html; do
+	{
+		git cat-file blob release:$file > tmp
+		grep -v '</body>' < tmp | grep -v '</html>'
+		echo "$TRACKER"
+		if grep -q '</body>' tmp; then
+			echo "</body></html>"
+		fi
+	} > "$DOCDIR/$file"
+done
+
+mv "$DOCDIR/README.html" tmp
+
+{
+	head -n 1 < tmp
+	cat <<EOF
+<h1>Tig Releases</h1>
+<p>Latest version of tig is <b>
+<a href="$LATEST.tar.gz">$LATEST</a>
+<a href="$LATEST.tar.gz.md5">(md5)</a></b>.</p>
+<p>See <a href="../NEWS.html">the release notes</a>
+for information about each release.</p>
+</div>
+$TRACKER
+EOF
+} > "$DOCDIR/releases/HEADER.html"
+
+head -n 2 < tmp > "$DOCDIR/HEADER.html"
+sed -n '3,$p' < tmp > "$DOCDIR/README.html"
+rm tmp
