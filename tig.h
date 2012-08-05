a232357c31686312c5fc83201bd9112d92b9072a
Douglas Livingstone <Douglas@redmelon.net>
2012-08-05 19:31:47 +0100

    Update AX_WITH_CURSES to build under Cygwin
    Serial 13 of AX_WITH_CURSES finds the Cygwin ncursesw package.
    
    This was fixed in: http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=commit;h=61c0c244e11d22ed5dc9a03895771309a515e482
    
    This also updates tig.h to use the headers suggested in the
    AX_WITH_CURSES documentation.

--- tig.h
+++ tig.h
@@ -59,10 +59,18 @@
 /* ncurses(3): Must be defined to have extended wide-character functions. */
 #define _XOPEN_SOURCE_EXTENDED
 
-#ifdef HAVE_NCURSESW_H
-#include <ncursesw/ncurses.h>
+#if defined HAVE_NCURSESW_CURSES_H
+#  include <ncursesw/curses.h>
+#elif defined HAVE_NCURSESW_H
+#  include <ncursesw.h>
+#elif defined HAVE_NCURSES_CURSES_H
+#  include <ncurses/curses.h>
+#elif defined HAVE_NCURSES_H
+#  include <ncurses.h>
+#elif defined HAVE_CURSES_H
+#  include <curses.h>
 #else
-#include <ncurses.h>
+#  error "SysV or X/Open-compatible Curses header file required"
 #endif
 
 #if __GNUC__ >= 3
