401d81cf7554859b3499881ed0a9355970ab1728
Samuel Bronson <naesten@gmail.com>
2012-09-06 21:52:40 -0400

    Work around a bug in autoreconf 2.61

---
+++ autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export WARNINGS=all
+set -e
+
+# Ideally, we could just do this:
+#
+#${AUTORECONF:-autoreconf} -v -I contrib
+#
+# Unfortunately, Autoconf 2.61's autoreconf(1) (found in Mac OS X 10.5
+# Leapard) neglects to pass the -I on to aclocal(1), which is
+# precisely where we need it!  So we do basically what it would have
+# done.
+
+run () {
+    echo $0: running: "$@"
+    "$@"
+}
+
+run ${ACLOCAL:-aclocal} -I contrib
+run ${AUTOCONF:-autoconf} --include=contrib
+run ${AUTOHEADER:-autoheader} --include=contrib
