jonas/tig

commit de7cb8abe84337fd5b628648dfda1b472e0a6c53

Eliminate a constant expression
Clang warns that the expression graph->position < 0 is always false
because ->position is unsigned.

graph.c
@@ -89,7 +89,6 @@ graph_needs_expansion(struct graph *graph)
 	if (graph->position + graph->parents.size > graph->row.size)
 		return TRUE;
 	return graph->parents.size > 1
-	    && graph->position < 0
 	    && graph->expanded < graph->parents.size;
 }
 

commit 4dd65a94a9b2d6918eb6af6ad2c8236bb6a2a9a7

Do not expand the graph for merges already aligned to the left
Fixes regression introduced in de7cb8abe84337fd5b628648dfda1b472e0a6c53
as part of GH-143.

graph.c
@@ -86,10 +86,11 @@ graph_add_parent(struct graph *graph, const char *parent)
 static bool
 graph_needs_expansion(struct graph *graph)
 {
-	if (graph->position + graph->parents.size > graph->row.size)
-		return TRUE;
+	return graph->position + graph->parents.size > graph->row.size;
+#if 0
 	return graph->parents.size > 1
 	    && graph->expanded < graph->parents.size;
+#endif
 }
 
 static bool