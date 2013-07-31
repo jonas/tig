jonas/tig

commit a0fdac2975ce8cf2de07435c7a3c6ccc1415393c

Create temporary file with name as suffix
This helps editors guess the filetype to properly set up their syntax
highlighting.

Signed-off-by: Sven Wegener <sven.wegener@stealer.net>
Signed-off-by: Jonas Fonseca <fonseca@diku.dk>

tig.c
@@ -5285,11 +5285,21 @@ tree_draw(struct view *view, struct line *line, unsigned int lineno)
 }
 
 static void
-open_blob_editor(const char *id, unsigned int lineno)
+open_blob_editor(const char *id, const char *name, unsigned int lineno)
 {
 	const char *blob_argv[] = { "git", "cat-file", "blob", id, NULL };
-	char file[SIZEOF_STR] = "/tmp/tigblob.XXXXXX";
-	int fd = mkstemp(file);
+	char file[SIZEOF_STR];
+	int fd;
+
+	if (!name)
+		name = "unknown";
+
+	if (!string_format(file, "/tmp/tigblob.XXXXXX.%s", name)) {
+		report("Temporary file name is too long");
+		return;
+	}
+
+	fd = mkstemps(file, strlen(name) + 1);
 
 	if (fd == -1)
 		report("Failed to create temporary file");
@@ -5321,7 +5331,7 @@ tree_request(struct view *view, enum request request, struct line *line)
 		if (line->type != LINE_TREE_FILE) {
 			report("Edit only supported for files");
 		} else if (!is_head_commit(view->vid)) {
-			open_blob_editor(entry->id, 0);
+			open_blob_editor(entry->id, entry->name, 0);
 		} else {
 			open_editor(opt_file, 0);
 		}
@@ -5518,7 +5528,7 @@ blob_request(struct view *view, enum request request, struct line *line)
 {
 	switch (request) {
 	case REQ_EDIT:
-		open_blob_editor(view->vid, (line - view->line) + 1);
+		open_blob_editor(view->vid, NULL, (line - view->line) + 1);
 		return REQ_NONE;
 	default:
 		return pager_request(view, request, line);

commit d7c81775db0d1978f8101535302870a4664fc5e6

Implement mkstemps() work-around for platforms lacking it
The function mkstemps() isn't available in all libc implementations. In
glibc it first became available in 2.11. This is likely true of many
non-LINUX platforms as well.

This work-around is taken from Git and was inspired by code in libiberty.

The files compat.[ch] have been created to hold this and other future
compatibility code.

This fixes breakage that was introduced with a0fdac29 "Create temporary
file with name as suffix."

Fixes #152

Signed-off-by: Drew Northup <n1xim.email@gmail.com>

Makefile
@@ -41,7 +41,7 @@ CFLAGS ?= -Wall -O2
 DFLAGS	= -g -DDEBUG -Werror -O0
 PROGS	= tig
 TESTS	= test-graph
-SOURCE	= tig.c tig.h io.c io.h graph.c graph.h
+SOURCE	= tig.c tig.h io.c io.h graph.c graph.h compat.c compat.h
 TXTDOC	= tig.1.txt tigrc.5.txt manual.txt NEWS README INSTALL BUGS
 MANDOC	= tig.1 tigrc.5 tigmanual.7
 HTMLDOC = tig.1.html tigrc.5.html manual.html README.html NEWS.html
@@ -168,11 +168,12 @@ configure: configure.ac acinclude.m4 contrib/*.m4
 
 graph.o: graph.c tig.h graph.h
 io.o: io.c tig.h io.h
+compat.o: compat.c compat.h tig.h
 refs.o: refs.c tig.h io.h refs.h
 test-graph.o: test-graph.c tig.h io.h graph.h
-tig.o: tig.c tig.h io.h refs.h graph.h git.h
+tig.o: tig.c tig.h compat.h io.h refs.h graph.h git.h
 
-tig: tig.o io.o graph.o refs.o
+tig: tig.o io.o graph.o refs.o compat.o
 test-graph: io.o graph.o
 
 # To check the above.

compat.c
@@ -1,0 +1,94 @@
+/* Copyright (c) 2006-2013 Jonas Fonseca <fonseca@diku.dk>
+ * Copyright (c) 2013 Drew Northup <n1xim.email@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "tig.h"
+#include "compat.h"
+
+/*
+ * Compatibility: no mkstemps()
+ * Adapted from libiberty's mkstemp.c via Git's wrapper.c.
+ */
+
+#undef TMP_MAX
+#define TMP_MAX 16384
+
+int tig_mkstemps_mode(char *pattern, int suffix_len, int mode)
+{
+	static const char letters[] =
+		"abcdefghijklmnopqrstuvwxyz"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"0123456789";
+	static const int num_letters = 62;
+	uint64_t value;
+	struct timeval tv;
+	char *template;
+	size_t len;
+	int fd, count;
+
+	len = strlen(pattern);
+
+	if (len < 6 + suffix_len) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (strncmp(&pattern[len - 6 - suffix_len], "XXXXXX", 6)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	/*
+	 * Replace pattern's XXXXXX characters with randomness.
+	 * Try TMP_MAX different filenames.
+	 */
+	gettimeofday(&tv, NULL);
+	value = ((size_t)(tv.tv_usec << 16)) ^ tv.tv_sec ^ getpid();
+	template = &pattern[len - 6 - suffix_len];
+	for (count = 0; count < TMP_MAX; ++count) {
+		uint64_t v = value;
+		/* Fill in the random bits. */
+		template[0] = letters[v % num_letters]; v /= num_letters;
+		template[1] = letters[v % num_letters]; v /= num_letters;
+		template[2] = letters[v % num_letters]; v /= num_letters;
+		template[3] = letters[v % num_letters]; v /= num_letters;
+		template[4] = letters[v % num_letters]; v /= num_letters;
+		template[5] = letters[v % num_letters]; v /= num_letters;
+
+		fd = open(pattern, O_CREAT | O_EXCL | O_RDWR, mode);
+		if (fd >= 0)
+			return fd;
+		/*
+		 * Fatal error (EPERM, ENOSPC etc).
+		 * It doesn't make sense to loop.
+		 */
+		if (errno != EEXIST)
+			break;
+		/*
+		 * This is a random value.  It is only necessary that
+		 * the next TMP_MAX values generated by adding 7777 to
+		 * VALUE are different with (modulo 2^32).
+		 */
+		value += 7777;
+	}
+	/* We return the null string if we can't find a unique file name.  */
+	pattern[0] = '\0';
+	return -1;
+}
+
+int tigmkstemps(char *pattern, int suffix_len)
+{
+	return tig_mkstemps_mode(pattern, suffix_len, 0600);
+}
+
+/* vim: set ts=8 sw=8 noexpandtab: */

compat.h
@@ -1,0 +1,37 @@
+/* Copyright (c) 2006-2013 Jonas Fonseca <fonseca@diku.dk>
+ * Copyright (c) 2013 Drew Northup <n1xim.email@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef TIG_COMPAT_H
+#define TIG_COMPAT_H
+
+#include "tig.h"
+
+/* Needed for mkstemps workaround */
+#include <stdint.h>
+
+/*
+ * Compatibility: no mkstemps()
+ * Adapted from libiberty via Git
+ */
+
+#ifndef HAVE_MKSTEMPS
+#define mkstemps tigmkstemps
+#endif
+
+int tigmkstemps(char *, int);
+int tig_mkstemps_mode(char *pattern, int suffix_len, int mode);
+
+#endif
+
+/* vim: set ts=8 sw=8 noexpandtab: */

configure.ac
@@ -21,6 +21,10 @@ AC_SUBST(CURSES_LIB)
 
 AM_ICONV
 
+dnl  Not all platforms have mkstemps
+AC_CHECK_FUNC([mkstemps], [AC_DEFINE([HAVE_MKSTEMPS], [1],
+	      [Define if mkstemps is available.])])
+
 AC_PROG_CC
 
 AC_CHECK_PROGS(ASCIIDOC, [asciidoc], [false])

tig.c
@@ -13,6 +13,7 @@
 
 #include "tig.h"
 #include "io.h"
+#include "compat.h"
 #include "refs.h"
 #include "graph.h"
 #include "git.h"