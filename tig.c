jonas/tig

commit a11bf779be32ba139e9c0839c50d12475581058c

Load HEAD ID and symbolic name as part of the repo info
This ensures that opt_remote is populated before the git config is
loaded so that tracking information for the current branch can be used
when highlighting remote references.

tig.c
@@ -8630,9 +8630,12 @@ load_git_config(void)
 #define REPO_INFO_WORK_TREE	"--is-inside-work-tree"
 #define REPO_INFO_SHOW_CDUP	"--show-cdup"
 #define REPO_INFO_SHOW_PREFIX	"--show-prefix"
+#define REPO_INFO_SYMBOLIC_HEAD	"--symbolic-full-name"
+#define REPO_INFO_RESOLVED_HEAD	"HEAD"
 
 struct repo_info_state {
 	const char **argv;
+	char head_id[SIZEOF_REV];
 };
 
 static int
@@ -8657,6 +8660,18 @@ read_repo_info(char *name, size_t namelen, char *value, size_t valuelen, void *d
 
 	} else if (!strcmp(arg, REPO_INFO_SHOW_PREFIX)) {
 		string_ncopy(opt_prefix, name, namelen);
+
+	} else if (!strcmp(arg, REPO_INFO_RESOLVED_HEAD)) {
+		string_ncopy(state->head_id, name, namelen);
+
+	} else if (!strcmp(arg, REPO_INFO_SYMBOLIC_HEAD)) {
+	    	if (!prefixcmp(name, "refs/heads/")) {
+			char *offset = name + STRING_SIZE("refs/heads/");
+
+			string_ncopy(opt_head, offset, strlen(offset) + 1);
+			add_ref(state->head_id, name, opt_remote, opt_head);
+		}
+		state->argv++;
 	}
 
 	return OK;
@@ -8666,10 +8681,11 @@ static int
 load_repo_info(void)
 {
 	const char *rev_parse_argv[] = {
-		"git", "rev-parse", REPO_INFO_GIT_DIR, REPO_INFO_WORK_TREE,
-			REPO_INFO_SHOW_CDUP, REPO_INFO_SHOW_PREFIX, NULL
+		"git", "rev-parse", REPO_INFO_RESOLVED_HEAD, REPO_INFO_SYMBOLIC_HEAD, "HEAD",
+			REPO_INFO_GIT_DIR, REPO_INFO_WORK_TREE, REPO_INFO_SHOW_CDUP,
+			REPO_INFO_SHOW_PREFIX, NULL
 	};
-	struct repo_info_state state = { rev_parse_argv + 2};
+	struct repo_info_state state = { rev_parse_argv + 2 };
 
 	return io_run_load(rev_parse_argv, "=", read_repo_info, &state);
 }

refs.c
@@ -103,9 +103,8 @@ struct ref_opt {
 };
 
 static int
-read_ref(char *id, size_t idlen, char *name, size_t namelen, void *data)
+add_to_refs(const char *id, size_t idlen, char *name, size_t namelen, struct ref_opt *opt)
 {
-	struct ref_opt *opt = data;
 	struct ref *ref = NULL;
 	bool tag = FALSE;
 	bool ltag = FALSE;
@@ -191,6 +190,12 @@ read_ref(char *id, size_t idlen, char *name, size_t namelen, void *data)
 	return OK;
 }
 
+static int
+read_ref(char *id, size_t idlen, char *name, size_t namelen, void *data)
+{
+	return add_to_refs(id, idlen, name, namelen, data);
+}
+
 int
 reload_refs(const char *git_dir, const char *remote_name, char *head, size_t headlen)
 {
@@ -245,6 +250,14 @@ reload_refs(const char *git_dir, const char *remote_name, char *head, size_t hea
 	qsort(refs, refs_size, sizeof(*refs), compare_refs);
 
 	return OK;
+}
+
+int
+add_ref(const char *id, char *name, const char *remote_name, const char *head)
+{
+	struct ref_opt opt = { remote_name, head };
+
+	return add_to_refs(id, strlen(id), name, strlen(name), &opt);
 }
 
 /* vim: set ts=8 sw=8 noexpandtab: */

refs.h
@@ -38,6 +38,7 @@ struct ref *get_ref_head();
 struct ref_list *get_ref_list(const char *id);
 void foreach_ref(bool (*visitor)(void *data, const struct ref *ref), void *data);
 int reload_refs(const char *git_dir, const char *remote_name, char *head, size_t headlen);
+int add_ref(const char *id, char *name, const char *remote_name, const char *head);
 
 #endif
 

commit 24c3c77a310b60a980638b9f204ff5301b9a8806

Fix regression making the status view inaccessible for new repositories
Commit a11bf779be32ba139e9c0839c50d12475581058c (Load HEAD ID and
symbolic name as part of the repo info) introduced the regression by
passing "HEAD" to git-rev-parse. In new repositories, HEAD does not
resolve to anything and thus caused an error message and no additional
output.

Work-around this issue by moving all reference arguments to the end of
the command.

tig.c
@@ -8719,9 +8719,10 @@ static int
 load_repo_info(void)
 {
 	const char *rev_parse_argv[] = {
-		"git", "rev-parse", REPO_INFO_RESOLVED_HEAD, REPO_INFO_SYMBOLIC_HEAD, "HEAD",
-			REPO_INFO_GIT_DIR, REPO_INFO_WORK_TREE, REPO_INFO_SHOW_CDUP,
-			REPO_INFO_SHOW_PREFIX, NULL
+		"git", "rev-parse", REPO_INFO_GIT_DIR, REPO_INFO_WORK_TREE,
+			REPO_INFO_SHOW_CDUP, REPO_INFO_SHOW_PREFIX, \
+			REPO_INFO_RESOLVED_HEAD, REPO_INFO_SYMBOLIC_HEAD, "HEAD",
+			NULL
 	};
 	struct repo_info_state state = { rev_parse_argv + 2 };
 