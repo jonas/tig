jonas/tig

commit 49632b8e5af0f0108195717acdf2a42c52a686f5

Move header files to include/tig
... to avoid conflicts with system headers.

include/tig/tig.h
@@ -15,7 +15,7 @@
 #define TIG_H
 
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+#include "tig/config.h"
 #endif
 
 #include "compat/compat.h"

commit ae8829311b62c1432e2d0b5118804a5b9c629c4a

Fix include of "config.h"
This fixes compilation when using configure. It was broken by commit
49632b8 which moved all Tig header files to include/tig.

Fixes #248

include/tig/tig.h
@@ -15,7 +15,7 @@
 #define TIG_H
 
 #ifdef HAVE_CONFIG_H
-#include "tig/config.h"
+#include "config.h"
 #endif
 
 #include "compat/compat.h"