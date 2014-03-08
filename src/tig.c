d0b2b352f2f4a4da4b551f845fea9fc771c3dff9
Jonas Fonseca <jonas.fonseca@gmail.com>
2014-03-08 11:28:50 -0500

    Extend key bindings for prompt commands to support predefined searches
    This adds support for binding keys to `:/<pattern>` and `:?<pattern>`,
    for example:
    
    	bind stage 2 :?^@@
    
    Based on this request by joelpet on stackoverflow:
     - http://stackoverflow.com/questions/22224598/find-previous-chunk-to-stage

--- src/tig.c
+++ src/tig.c
@@ -673,6 +673,10 @@ run_prompt_command(struct view *view, char *cmd)
 
 		return get_keybinding(&view->ops->keymap, &input);
 
+	} else if (cmd[0] == '/' || cmd[0] == '?') {
+		string_ncopy(view->env->search, cmd + 1, strlen(cmd + 1));
+		return cmd[0] == '/' ? REQ_FIND_NEXT : REQ_FIND_PREV;
+
 	} else if (cmd[0] == '!') {
 		struct view *next = VIEW(REQ_VIEW_PAGER);
 		const char *argv[SIZEOF_ARG];
