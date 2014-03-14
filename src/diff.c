jonas/tig

commit dd3713a7db320b49816676e9423ee031fbac3745

Fix blame opening from (un)staged changes diff view
When showing the diff of staged or unstaged changes commit opened from
the main view, the commit ID is the magic all-zero SHA (00000...)  which
caused git-blame to choke. Detect this so that blame can be opened even
from these diff views.

src/diff.c
@@ -348,7 +348,10 @@ diff_trace_origin(struct view *view, struct line *line)
 	else
 		string_format(ref, "%s^", view->vid);
 
-	if (!diff_blame_line(ref, file, lineno, &header, &commit)) {
+	if (string_rev_is_null(ref)) {
+		header.id[0] = 0;
+		header.orig_lineno = lineno;
+	} else if (!diff_blame_line(ref, file, lineno, &header, &commit)) {
 		report("Failed to read blame data");
 		return REQ_NONE;
 	}

commit 6ca344f3c4f7830dcb77f1a653beb39b909f2cc0

Fix blame opening from (un)staged changes diff view (part II)
This fixes a regression in dd3713a7db320b49816676e9423ee031fbac3745 due
to memory corruption. The nullable SHA ID is now completely special
cased.

src/diff.c
@@ -349,16 +349,20 @@ diff_trace_origin(struct view *view, struct line *line)
 		string_format(ref, "%s^", view->vid);
 
 	if (string_rev_is_null(ref)) {
-		header.id[0] = 0;
-		header.orig_lineno = lineno;
-	} else if (!diff_blame_line(ref, file, lineno, &header, &commit)) {
-		report("Failed to read blame data");
-		return REQ_NONE;
-	}
+		string_ncopy(view->env->file, file, strlen(file));
+		string_copy(view->env->ref, "");
+		view->env->lineno = lineno - 1;
 
-	string_ncopy(view->env->file, commit.filename, strlen(commit.filename));
-	string_copy(view->env->ref, header.id);
-	view->env->lineno = header.orig_lineno - 1;
+	} else {
+		if (!diff_blame_line(ref, file, lineno, &header, &commit)) {
+			report("Failed to read blame data");
+			return REQ_NONE;
+		}
+
+		string_ncopy(view->env->file, commit.filename, strlen(commit.filename));
+		string_copy(view->env->ref, header.id);
+		view->env->lineno = header.orig_lineno - 1;
+	}
 
 	return REQ_VIEW_BLAME;
 }