3cfa9ad96a7e3a1b07737cf8290cea60d120313c
Jonas Fonseca <jonas.fonseca@gmail.com>
2014-05-08 21:50:53 -0400

    Fix release script to work for patch versions

--- tools/release.sh
+++ tools/release.sh
@@ -34,7 +34,7 @@ if test -n "$VERSION"; then
 		git checkout master
 
 	# Update files which should reference the version.
-	sed -i "s/VERSION\s=\s[0-9]\+[.][0-9]\+/VERSION	= $VERSION/" Makefile	
+	sed -i "s/VERSION\s=\s[0-9]\+\([.][0-9]\+\)\+/VERSION	= $VERSION/" Makefile	
 	perl -pi -e 's/^tig master.*/@@TITLE@@/ms' "$NEWS"
 	perl -pi -e "s/^@@TITLE@@.*/$TITLE/" "$NEWS"
 
