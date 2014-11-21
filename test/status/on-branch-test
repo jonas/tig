jonas/tig

commit 3615bdee4c3d1a0c719fc6b24f25f5745a6f2d92

Fix and improve status on-branch information
Add a test for the different on-branch "modes" based on whether
git-rebase, git-am or git-merge is in progress or whether a detached
head has been checked out.

This also changes status view refreshing to only reload the current
branch information and not all refs as suggested in #341. Note that it
actually reloads all repo information to get both the symbolic ref
pointed to by HEAD as well as the HEAD id.

src/repo.c
@@ -26,7 +26,6 @@
 
 struct repo_info_state {
 	const char **argv;
-	char head_id[SIZEOF_REV];
 };
 
 static int
@@ -61,19 +60,26 @@ read_repo_info(char *name, size_t namelen, char *value, size_t valuelen, void *d
 		string_ncopy(repo.prefix, name, namelen);
 
 	} else if (!strcmp(arg, REPO_INFO_RESOLVED_HEAD)) {
-		string_ncopy(state->head_id, name, namelen);
+		string_ncopy(repo.head_id, name, namelen);
 
 	} else if (!strcmp(arg, REPO_INFO_SYMBOLIC_HEAD)) {
 		if (!prefixcmp(name, "refs/heads/")) {
-			char *offset = name + STRING_SIZE("refs/heads/");
-
-			string_ncopy(repo.head, offset, strlen(offset) + 1);
-			add_ref(state->head_id, name, repo.remote, repo.head);
+			add_ref(repo.head_id, name, repo.remote, repo.head);
+			name += STRING_SIZE("refs/heads/");
 		}
+		string_ncopy(repo.head, name, strlen(name) + 1);
 		state->argv++;
 	}
 
 	return OK;
+}
+
+static int
+reload_repo_info(const char **rev_parse_argv)
+{
+	struct repo_info_state state = { rev_parse_argv + 2 };
+
+	return io_run_load(rev_parse_argv, "=", read_repo_info, &state);
 }
 
 int
@@ -85,9 +91,19 @@ load_repo_info(void)
 			REPO_INFO_RESOLVED_HEAD, REPO_INFO_SYMBOLIC_HEAD, "HEAD",
 			NULL
 	};
-	struct repo_info_state state = { rev_parse_argv + 2 };
 
-	return io_run_load(rev_parse_argv, "=", read_repo_info, &state);
+	return reload_repo_info(rev_parse_argv);
+}
+
+int
+load_repo_head(void)
+{
+	const char *rev_parse_argv[] = {
+		"git", "rev-parse", REPO_INFO_RESOLVED_HEAD,
+			REPO_INFO_SYMBOLIC_HEAD, "HEAD", NULL
+	};
+
+	return reload_repo_info(rev_parse_argv);
 }
 
 struct repo_info repo;

src/status.c
@@ -201,15 +201,15 @@ status_restore(struct view *view)
 static void
 status_update_onbranch(void)
 {
-	static const char *paths[][2] = {
-		{ "rebase-apply/rebasing",	"Rebasing" },
-		{ "rebase-apply/applying",	"Applying mailbox" },
-		{ "rebase-apply/",		"Rebasing mailbox" },
-		{ "rebase-merge/interactive",	"Interactive rebase" },
-		{ "rebase-merge/",		"Rebase merge" },
-		{ "MERGE_HEAD",			"Merging" },
-		{ "BISECT_LOG",			"Bisecting" },
-		{ "HEAD",			"On branch" },
+	static const char *paths[][3] = {
+		{ "rebase-apply/rebasing",	"rebase-apply/head-name",	"Rebasing" },
+		{ "rebase-apply/applying",	"rebase-apply/head-name",	"Applying mailbox to" },
+		{ "rebase-apply/",		"rebase-apply/head-name",	"Rebasing mailbox onto" },
+		{ "rebase-merge/interactive",	"rebase-merge/head-name",	"Interactive rebase" },
+		{ "rebase-merge/",		"rebase-merge/head-name",	"Rebase merge" },
+		{ "MERGE_HEAD",			NULL,				"Merging" },
+		{ "BISECT_LOG",			NULL,				"Bisecting" },
+		{ "HEAD",			NULL,				"On branch" },
 	};
 	char buf[SIZEOF_STR];
 	struct stat stat;
@@ -221,16 +221,17 @@ status_update_onbranch(void)
 	}
 
 	for (i = 0; i < ARRAY_SIZE(paths); i++) {
+		const char *prefix = paths[i][2];
 		char *head = repo.head;
 
 		if (!string_format(buf, "%s/%s", repo.git_dir, paths[i][0]) ||
 		    lstat(buf, &stat) < 0)
 			continue;
 
-		if (!*repo.head) {
+		if (paths[i][1]) {
 			struct io io;
 
-			if (io_open(&io, "%s/rebase-merge/head-name", repo.git_dir) &&
+			if (io_open(&io, "%s/%s", repo.git_dir, paths[i][1]) &&
 			    io_read_buf(&io, buf, sizeof(buf))) {
 				head = buf;
 				if (!prefixcmp(head, "refs/heads/"))
@@ -238,7 +239,12 @@ status_update_onbranch(void)
 			}
 		}
 
-		if (!string_format(status_onbranch, "%s %s", paths[i][1], head))
+		if (!strcmp(head, "HEAD") && !strcmp(paths[i][0], "HEAD") && *repo.head_id) {
+			prefix = "On detached head";
+			head = repo.head_id;
+		}
+
+		if (!string_format(status_onbranch, "%s %s", prefix, head))
 			string_copy(status_onbranch, repo.head);
 		return;
 	}
@@ -633,7 +639,7 @@ status_request(struct view *view, enum request request, struct line *line)
 
 	case REQ_REFRESH:
 		/* Load the current branch information and then the view. */
-		load_refs(TRUE);
+		load_repo_head();
 		break;
 
 	default:

test/status/on-branch-test
@@ -1,0 +1,143 @@
+#!/bin/sh
+
+. libtest.sh
+. libgit.sh
+
+export LINES=10
+
+steps "
+	:save-display on-branch-master.screen
+
+	:exec @git bisect start master
+	:save-display bisecting-master.screen
+	:exec @git bisect reset
+
+	:exec @git checkout HEAD~10
+	:save-display on-detached-head.screen
+	:exec @git checkout master
+
+	:exec @git merge conflict-branch
+	:save-display merging-master.screen
+	:exec @git reset --hard
+
+	:exec @git checkout conflict-branch
+	:exec @git rebase master
+	:save-display rebasing-master.screen
+	:exec @git rebase --abort
+	:exec @git checkout master
+
+	:exec @git am $HOME/mailbox
+	:save-display applying-mailbox-to-master.screen
+	:exec @git am --abort
+
+	:exec @git checkout conflict-branch
+	:exec @git rebase -i master
+	:save-display interactive-rebase-of-branch.screen
+	:exec @git rebase --abort
+	:exec @git checkout master
+"
+
+prepare_repo()
+{
+	git checkout -b topic-branch HEAD~4
+	echo "Topic branch" >> README.md
+	git add README.md
+	git_commit -m "Topic branch"
+	git checkout master
+
+	git checkout -b conflict-branch HEAD~4
+	git rm -f run.sh
+	git_commit -m "Conflict branch"
+	git checkout master
+
+	git format-patch -1 conflict-branch --stdout > "$HOME/mailbox"
+}
+
+in_work_dir create_repo_from_tgz "$base_dir/files/scala-js-benchmarks.tgz"
+in_work_dir prepare_repo 1>>"$HOME/prepare-repo.log" 2>>"$HOME/prepare-repo.log"
+
+test_tig status
+
+assert_equals 'on-branch-master.screen' <<EOF
+On branch master                                                                
+Changes to be committed:                                                        
+  (no files)                                                                    
+Changed but not updated:                                                        
+  (no files)                                                                    
+Untracked files:                                                                
+  (no files)                                                                    
+                                                                                
+[status] Nothing to update                                                  100%
+EOF
+
+assert_equals 'bisecting-master.screen' <<EOF
+Bisecting master                                                                
+Changes to be committed:                                                        
+  (no files)                                                                    
+Changed but not updated:                                                        
+  (no files)                                                                    
+Untracked files:                                                                
+  (no files)                                                                    
+                                                                                
+[status] Nothing to update                                                  100%
+EOF
+
+assert_equals 'on-detached-head.screen' <<EOF
+On detached head 988c77aad5798f1e087e5d7efe3b3319ee6de744                       
+Changes to be committed:                                                        
+  (no files)                                                                    
+Changed but not updated:                                                        
+  (no files)                                                                    
+Untracked files:                                                                
+  (no files)                                                                    
+                                                                                
+[status] Nothing to update                                                  100%
+EOF
+
+assert_equals 'merging-master.screen' <<EOF
+Merging master                                                                  
+Changes to be committed:                                                        
+  (no files)                                                                    
+Changed but not updated:                                                        
+U run.sh                                                                        
+Untracked files:                                                                
+  (no files)                                                                    
+                                                                                
+[status] Nothing to update                                                  100%
+EOF
+
+assert_equals 'rebasing-master.screen' <<EOF
+Rebasing conflict-branch                                                        
+Changes to be committed:                                                        
+  (no files)                                                                    
+Changed but not updated:                                                        
+U run.sh                                                                        
+Untracked files:                                                                
+  (no files)                                                                    
+                                                                                
+[status] Nothing to update                                                  100%
+EOF
+
+assert_equals 'applying-mailbox-to-master.screen' <<EOF
+Applying mailbox to master
+Changes to be committed:                                                        
+  (no files)                                                                    
+Changed but not updated:                                                        
+  (no files)                                                                    
+Untracked files:                                                                
+  (no files)                                                                    
+                                                                                
+[status] Nothing to update                                                  100%
+EOF
+
+assert_equals 'interactive-rebase-of-branch.screen' << EOF
+Interactive rebase conflict-branch                                              
+Changes to be committed:                                                        
+  (no files)                                                                    
+Changed but not updated:                                                        
+U run.sh                                                                        
+Untracked files:                                                                
+  (no files)                                                                    
+                                                                                
+[status] Nothing to update                                                  100%
+EOF

include/tig/repo.h
@@ -18,6 +18,7 @@
 
 struct repo_info {
 	char head[SIZEOF_REF];
+	char head_id[SIZEOF_REV];
 	char remote[SIZEOF_REF];
 	char cdup[SIZEOF_STR];
 	char prefix[SIZEOF_STR];
@@ -28,6 +29,7 @@ struct repo_info {
 extern struct repo_info repo;
 
 int load_repo_info(void);
+int load_repo_head(void);
 
 struct index_diff {
 	int staged;

commit 1f6e98b0d5af038ccf4beef6e39252c8eb0f2afe

Fix status view regression where tracked files was shown as added
The changes in 3615bdee4c3d1a0c719fc6b24f25f5745a6f2d92 introduced the
regression. Since it only occurs when `tig status` is run in a repo with
a detached head it was not caught by status/on-branch-test. To rectify
this on-branch-test now checks each individual test case as well as
running them all together.

src/repo.c
@@ -66,8 +66,8 @@ read_repo_info(char *name, size_t namelen, char *value, size_t valuelen, void *d
 		if (!prefixcmp(name, "refs/heads/")) {
 			add_ref(repo.head_id, name, repo.remote, repo.head);
 			name += STRING_SIZE("refs/heads/");
+			string_ncopy(repo.head, name, strlen(name) + 1);
 		}
-		string_ncopy(repo.head, name, strlen(name) + 1);
 		state->argv++;
 	}
 
@@ -92,6 +92,7 @@ load_repo_info(void)
 			NULL
 	};
 
+	memset(&repo, 0, sizeof(repo));
 	return reload_repo_info(rev_parse_argv);
 }
 
@@ -103,6 +104,8 @@ load_repo_head(void)
 			REPO_INFO_SYMBOLIC_HEAD, "HEAD", NULL
 	};
 
+	memset(repo.head, 0, sizeof(repo.head));
+	memset(repo.head_id, 0, sizeof(repo.head_id));
 	return reload_repo_info(rev_parse_argv);
 }
 

src/status.c
@@ -239,7 +239,7 @@ status_update_onbranch(void)
 			}
 		}
 
-		if (!strcmp(head, "HEAD") && !strcmp(paths[i][0], "HEAD") && *repo.head_id) {
+		if (!*head && !strcmp(paths[i][0], "HEAD") && *repo.head_id) {
 			prefix = "On detached head";
 			head = repo.head_id;
 		}

test/refs/branch-checkout-test
@@ -20,6 +20,7 @@ steps '
 	:exec @sh -c '\''printf "refs=%s\nbranch=%s\ntag=%s\n" "%(ref)" "%(branch)" "%(tag)" > $HOME/%(commit).refinfo'\''
 	:exec @git checkout -q %(tag)
 	:save-display refs-checked-out-tag.screen
+	:7
 	:exec @sh -c "git rev-parse HEAD > $HOME/%(ref).head"
 '
 
@@ -84,11 +85,11 @@ EOF
 
 assert_equals 'refs-checked-out-tag.screen' <<EOF
                                All references                                             
+2010-01-20 14:18 A. U. Thor    HEAD                                                       
 2010-04-07 05:37 Max Power     master                                                     
 2010-03-04 04:09 A. U. Thor    this_is_really_a_long_long_long_long_long_long_long_branch 
 2010-04-07 05:37 Max Power     origin/master                                              
 2010-04-07 05:37 Max Power     origin/HEAD                                                
 2010-01-20 14:18 A. U. Thor    this_is_really_a_long_long_long_long_long_long_long_tag    
-2009-12-17 12:49 René Lévesque v1.0                                                     
-[refs] a3f25ca556c1cef1ef3a0704f5deaec8687c84b9 - reference 5 of 6                    100%
+[refs] 5cb3412a5e06e506840495b91acc885037a48b72 - reference 5 of 7                     87%
 EOF

test/status/on-branch-test
@@ -5,37 +5,13 @@
 
 export LINES=10
 
-steps "
-	:save-display on-branch-master.screen
-
-	:exec @git bisect start master
-	:save-display bisecting-master.screen
-	:exec @git bisect reset
-
-	:exec @git checkout HEAD~10
-	:save-display on-detached-head.screen
-	:exec @git checkout master
-
-	:exec @git merge conflict-branch
-	:save-display merging-master.screen
-	:exec @git reset --hard
-
-	:exec @git checkout conflict-branch
-	:exec @git rebase master
-	:save-display rebasing-master.screen
-	:exec @git rebase --abort
-	:exec @git checkout master
-
-	:exec @git am $HOME/mailbox
-	:save-display applying-mailbox-to-master.screen
-	:exec @git am --abort
-
-	:exec @git checkout conflict-branch
-	:exec @git rebase -i master
-	:save-display interactive-rebase-of-branch.screen
-	:exec @git rebase --abort
-	:exec @git checkout master
-"
+run_prep()
+{
+	echo "=== $@ ===" > "$HOME/prepare-repo.log"
+	set +e
+	in_work_dir "$@" 1>>"$HOME/prepare-repo.log" 2>>"$HOME/prepare-repo.log"
+	set -e
+}
 
 prepare_repo()
 {
@@ -54,11 +30,23 @@ prepare_repo()
 }
 
 in_work_dir create_repo_from_tgz "$base_dir/files/scala-js-benchmarks.tgz"
-in_work_dir prepare_repo 1>>"$HOME/prepare-repo.log" 2>>"$HOME/prepare-repo.log"
+run_prep prepare_repo
 
-test_tig status
+test_case()
+{
+	name="$1";
+	before="$2";
+	after="$3";
 
-assert_equals 'on-branch-master.screen' <<EOF
+	echo "$name" >> test-cases
+	echo "$before" > "$name-before.sh"
+	echo "$after" > "$name-after.sh"
+	cat > "$name.expected"
+}
+
+test_case on-branch-master \
+	'git checkout master' \
+	'' <<EOF
 On branch master                                                                
 Changes to be committed:                                                        
   (no files)                                                                    
@@ -70,7 +58,9 @@ Untracked files:
 [status] Nothing to update                                                  100%
 EOF
 
-assert_equals 'bisecting-master.screen' <<EOF
+test_case bisecting-master \
+	"git bisect start master" \
+	"git bisect reset" <<EOF
 Bisecting master                                                                
 Changes to be committed:                                                        
   (no files)                                                                    
@@ -82,7 +72,10 @@ Untracked files:
 [status] Nothing to update                                                  100%
 EOF
 
-assert_equals 'on-detached-head.screen' <<EOF
+test_case on-detached-head \
+	"git checkout HEAD~10" \
+	"git checkout master" \
+<<EOF
 On detached head 988c77aad5798f1e087e5d7efe3b3319ee6de744                       
 Changes to be committed:                                                        
   (no files)                                                                    
@@ -94,7 +87,10 @@ Untracked files:
 [status] Nothing to update                                                  100%
 EOF
 
-assert_equals 'merging-master.screen' <<EOF
+test_case merging-master \
+	"git merge conflict-branch" \
+	"git reset --hard" \
+<<EOF
 Merging master                                                                  
 Changes to be committed:                                                        
   (no files)                                                                    
@@ -106,7 +102,12 @@ Untracked files:
 [status] Nothing to update                                                  100%
 EOF
 
-assert_equals 'rebasing-master.screen' <<EOF
+test_case rebasing-master \
+	"git checkout conflict-branch
+	 git rebase master" \
+	"git rebase --abort
+	 git checkout master" \
+<<EOF
 Rebasing conflict-branch                                                        
 Changes to be committed:                                                        
   (no files)                                                                    
@@ -118,7 +119,10 @@ Untracked files:
 [status] Nothing to update                                                  100%
 EOF
 
-assert_equals 'applying-mailbox-to-master.screen' <<EOF
+test_case applying-mailbox-to-master \
+	"git am $HOME/mailbox" \
+	"git am --abort" \
+<<EOF
 Applying mailbox to master
 Changes to be committed:                                                        
   (no files)                                                                    
@@ -130,7 +134,12 @@ Untracked files:
 [status] Nothing to update                                                  100%
 EOF
 
-assert_equals 'interactive-rebase-of-branch.screen' << EOF
+test_case interactive-rebase-of-branch \
+	"git checkout conflict-branch
+	 git rebase -i master" \
+	"git rebase --abort
+	 git checkout master" \
+<<EOF
 Interactive rebase conflict-branch                                              
 Changes to be committed:                                                        
   (no files)                                                                    
@@ -141,3 +150,37 @@ Untracked files:
                                                                                 
 [status] Nothing to update                                                  100%
 EOF
+
+for name in $(cat test-cases); do
+	tig_script "$name.script" "
+		:save-display $name.screen
+	"
+	run_prep sh "$HOME/$name-before.sh" 
+	(cd "$work_dir" && tig status) >"$name.stdout" 2>"$name.stderr"
+	run_prep sh "$HOME/$name-after.sh" 
+
+	assert_equals "$name.screen" < "$name.expected"
+	assert_equals "$name.stderr" ''
+done
+
+add_exec_prefix()
+{
+	code="$(cat "$1")"
+	if [ -n "$code" ]; then
+		echo "$code" | sed -e 's/^[ 	]*//' -e '/^$/d' -e 's/^/:exec @/'
+	fi
+}
+
+tig_script "all.script" "
+	$(for name in $(cat test-cases); do
+		add_exec_prefix "$name-before.sh"
+		echo :save-display all-$name.screen
+		add_exec_prefix "$name-after.sh"
+	done)
+"
+
+test_tig status
+
+for name in $(cat test-cases); do
+	assert_equals "all-$name.screen" < "$name.expected"
+done

test/tools/libtest.sh
@@ -102,7 +102,7 @@ file() {
 	path="$1"; shift
 
 	mkdir -p "$(dirname "$path")"
-	if [ -z "$@" ]; then
+	if [ "$#" = 0 ]; then
 		case "$path" in
 			stdin|expected*) cat ;;
 			*) sed 's/^[ ]//' ;;