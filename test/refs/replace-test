jonas/tig

commit c9bf04eb66c9bf064331235d964cdd8920b1156a

Fix string_map_remove to not use the hashtab INSERT mode

src/refdb.c
@@ -149,11 +149,9 @@ add_to_refs(const char *id, size_t idlen, char *name, size_t namelen, struct ref
 	 * git-ls-remote lists the commit id of an annotated tag right
 	 * before the commit id it points to. */
 	if (type == REFERENCE_REPLACE) {
-		ref_slot = string_map_put_to(&refs_by_id, id);
-		if (!ref_slot)
+		ref = string_map_remove(&refs_by_id, id);
+		if (!ref)
 			return ERR;
-		if (*ref_slot)
-			ref = string_map_remove(&refs_by_id, ref_slot);
 
 	} else {
 		ref_slot = string_map_put_to(&refs_by_name, name);

include/tig/map.h
@@ -37,7 +37,7 @@ extern string_map_hash_fn string_map_hash_helper;
 void *string_map_get(struct string_map *map, const char *key);
 void *string_map_put(struct string_map *map, const char *key, void *value);
 void **string_map_put_to(struct string_map *map, const char *key);
-void *string_map_remove(struct string_map *map, void **slot);
+void *string_map_remove(struct string_map *map, const char *key);
 void string_map_clear(struct string_map *map);
 void string_map_foreach(struct string_map *map, string_map_iterator_fn iterator, void *data);
 

src/map.c
@@ -65,12 +65,20 @@ string_map_put(struct string_map *map, const char *key, void *value)
 }
 
 void *
-string_map_remove(struct string_map *map, void **slot)
+string_map_remove(struct string_map *map, const char *key)
 {
-	void *value = slot ? *slot : NULL;
+	void *value = NULL;
+	void **slot;
 
-	if (map->htab && slot)
+	if (!map->htab)
+		return NULL;
+
+	map->key = key;
+	slot = htab_find_slot_with_hash(map->htab, map, htab_hash_string(key), NO_INSERT);
+	if (slot) {
+		value = *slot;
 		htab_clear_slot(map->htab, slot);
+	}
 
 	return value;
 }

commit 6b7ddaff8266c8273f7b66849bd00e5185b52521

Ignore replaced refs for unknown IDs
Restore the old behavior where replaced refs are ignored unless they map
to an existing ID. The regression was introduced in commit
c9bf04eb66c9bf064331235d964cdd8920b1156a.

Fixes #374

src/refdb.c
@@ -124,7 +124,7 @@ add_to_refs(const char *id, size_t idlen, char *name, size_t namelen, struct ref
 	struct ref *ref = NULL;
 	enum reference_type type = REFERENCE_BRANCH;
 	void **ref_lists_slot;
-	void **ref_slot;
+	void **ref_slot = NULL;
 
 	if (!prefixcmp(name, "refs/tags/")) {
 		type = REFERENCE_TAG;
@@ -173,8 +173,6 @@ add_to_refs(const char *id, size_t idlen, char *name, size_t namelen, struct ref
 	 * before the commit id it points to. */
 	if (type == REFERENCE_REPLACE) {
 		ref = string_map_remove(&refs_by_id, id);
-		if (!ref)
-			return ERR;
 
 	} else {
 		ref_slot = string_map_put_to(&refs_by_name, name);
@@ -188,7 +186,8 @@ add_to_refs(const char *id, size_t idlen, char *name, size_t namelen, struct ref
 		if (!ref)
 			return ERR;
 		strncpy(ref->name, name, namelen);
-		*ref_slot = ref;
+		if (ref_slot)
+			*ref_slot = ref;
 	}
 
 	if (strncmp(ref->id, id, idlen) || ref->type != type) {

test/refs/replace-test
@@ -25,8 +25,16 @@ prepare_repo()
 	replaced_id=$(git rev-parse HEAD~10)
 	replacement_id=$(git rev-parse HEAD~20)
 
+	replaced_no_branch_1_id=$(git rev-parse HEAD~22)
+	replacement_no_branch_1_id=$(git rev-parse HEAD~23)
+
+	replaced_no_branch_2_id=$(git rev-parse HEAD~25)
+	replacement_no_branch_2_id=$(git rev-parse HEAD~30)
+
 	git branch not-replace "$replaced_id"
 	git replace "$replaced_id" "$replacement_id"
+	git replace "$replaced_no_branch_1_id" "$replacement_no_branch_1_id"
+	git replace "$replaced_no_branch_2_id" "$replacement_no_branch_2_id"
 }
 
 in_work_dir prepare_repo
@@ -66,9 +74,9 @@ assert_equals 'main.screen' <<EOF
 a3f25ca 2010-01-20 14:18 A. U. Thor            o Commit 9 A                     
 d9a8c8b 2009-10-18 22:14 Max Power             o ~not-replace~ Commit 6 E       
 02e1e72 2009-10-10 09:52 Jørgen Thygesen Brahe o Commit 6 D                    
-5d9d565 2009-10-01 21:30 作者                  o Commit 6 C                   
-257a506 2009-09-23 09:07 René Lévesque         o Commit 6 B                   
+5d9d565 2009-09-23 09:07 René Lévesque         o ~replaced~ Commit 6 B        
 2ba9ed5 2009-09-14 20:45 A. U. Thor            o Commit 6 A                     
-2053940 2009-09-06 08:23 Max Power             o Commit 5 E                     
-[main] 5cb3412a5e06e506840495b91acc885037a48b72 - commit 1 of 40             40%
+2053940 2009-07-25 18:32 Max Power             o ~replaced~ Commit 4 E          
+ceac6c2 2009-07-17 06:10 Jørgen Thygesen Brahe o Commit 4 D                    
+[main] 5cb3412a5e06e506840495b91acc885037a48b72 - commit 1 of 34             47%
 EOF