jonas/tig

commit f0c1f17e7a824dd779028032a5c9240a75218bde

Undefine TRUE and FALSE to force C99
Catch and convert remaining usage of ncurses FALSE and TRUE to C99 bool.
Change all calls to setsyx() to move() since setsyx() is defined as a
macro and uses FALSE and TRUE.

src/display.c
@@ -120,7 +120,7 @@ open_editor(const char *file, unsigned int lineno)
 	if (lineno && opt_editor_line_number && string_format(lineno_cmd, "+%u", lineno))
 		editor_argv[argc++] = lineno_cmd;
 	editor_argv[argc] = file;
-	if (!open_external_viewer(editor_argv, repo.cdup, false, FALSE, true, EDITOR_LINENO_MSG))
+	if (!open_external_viewer(editor_argv, repo.cdup, false, false, true, EDITOR_LINENO_MSG))
 		opt_editor_line_number = false;
 }
 
@@ -588,7 +588,7 @@ get_input(int prompt_position, struct key *key)
 			cursor_x += view->width - 1;
 			cursor_y += view->pos.lineno - view->pos.offset;
 		}
-		setsyx(cursor_y, cursor_x);
+		move(cursor_y, cursor_x);
 
 		if (is_script_executing()) {
 			/* Wait for the current command to complete. */

src/status.c
@@ -505,7 +505,7 @@ status_update_files(struct view *view, struct line *line)
 			string_format(view->ref, "updating file %u of %u (%d%% done)",
 				      file, files, done);
 			update_view_title(view);
-			setsyx(cursor_y, cursor_x);
+			move(cursor_y, cursor_x);
 			doupdate();
 		}
 		result = status_update_write(&io, line->data, line->type);

include/tig/tig.h
@@ -82,6 +82,13 @@
 #  include <curses.h>
 #endif
 
+#ifdef TRUE
+#undef TRUE
+#endif
+#ifdef FALSE
+#undef FALSE
+#endif
+
 #if __GNUC__ >= 3
 #define TIG_NORETURN __attribute__((__noreturn__))
 #define PRINTF_LIKE(fmt, args) __attribute__((format (printf, fmt, args)))

src/keys.c
@@ -603,7 +603,7 @@ foreach_key(key_visitor_fn visitor, void *data, bool combine_keys)
 		if (!foreach_request(foreach_key_request, &state)
 		    || !foreach_key_run_request(&state, true, true)
 		    || !foreach_key_run_request(&state, true, false)
-		    || !foreach_key_run_request(&state, false, FALSE))
+		    || !foreach_key_run_request(&state, false, false))
 			return false;
 	}
 

src/line.c
@@ -165,10 +165,10 @@ foreach_line_rule(line_rule_visitor_fn visitor, void *data)
 		struct line_rule *rule = &line_rule[type];
 
 		if (!visitor(data, rule))
-			return FALSE;
+			return false;
 	}
 
-	return TRUE;
+	return true;
 }
 
 static void

src/prompt.c
@@ -134,7 +134,7 @@ bool
 prompt_yesno(const char *prompt)
 {
 	char prompt2[SIZEOF_STR];
-	struct input input = { prompt_yesno_handler, FALSE, NULL };
+	struct input input = { prompt_yesno_handler, false, NULL };
 
 	if (!string_format(prompt2, "%s [Yy/Nn]", prompt))
 		return false;

commit 9b82765d51e517d81f4b3c10c62e9a13f1d6c3ea

Revisit ban on FALSE and TRUE to also replace getsyx
Commit f0c1f17e7a824dd779028032a5c9240a75218bde broke the build on Linux
(via Travis) where is_leaveok() uses FALSE. Fix this by added proper
replacement macros for setsyx() and getsyx() called set_cursor_pos() and
get_cursor_pos().

include/tig/display.h
@@ -53,5 +53,8 @@ void enable_mouse(bool enable);
 
 enum status_code open_script(const char *path);
 
+#define get_cursor_pos(cursor_y, cursor_x) getyx(newscr, cursor_y, cursor_x)
+#define set_cursor_pos(cursor_y, cursor_x) wmove(newscr, cursor_y, cursor_x)
+
 #endif
 /* vim: set ts=8 sw=8 noexpandtab: */

src/display.c
@@ -588,7 +588,7 @@ get_input(int prompt_position, struct key *key)
 			cursor_x += view->width - 1;
 			cursor_y += view->pos.lineno - view->pos.offset;
 		}
-		move(cursor_y, cursor_x);
+		set_cursor_pos(cursor_y, cursor_x);
 
 		if (is_script_executing()) {
 			/* Wait for the current command to complete. */

src/status.c
@@ -496,7 +496,7 @@ status_update_files(struct view *view, struct line *line)
 		files++;
 
 	string_copy(buf, view->ref);
-	getsyx(cursor_y, cursor_x);
+	get_cursor_pos(cursor_y, cursor_x);
 	for (file = 0, done = 5; result && file < files; line++, file++) {
 		int almost_done = file * 100 / files;
 
@@ -505,7 +505,7 @@ status_update_files(struct view *view, struct line *line)
 			string_format(view->ref, "updating file %u of %u (%d%% done)",
 				      file, files, done);
 			update_view_title(view);
-			move(cursor_y, cursor_x);
+			set_cursor_pos(cursor_y, cursor_x);
 			doupdate();
 		}
 		result = status_update_write(&io, line->data, line->type);