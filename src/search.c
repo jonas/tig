jonas/tig

commit 1be122802b50b31d3cb1402d06e7f649ae2386d8

Make search_view prompt for search string

src/search.c
@@ -12,6 +12,7 @@
  */
 
 #include "tig/search.h"
+#include "tig/prompt.h"
 #include "tig/display.h"
 #include "tig/draw.h"
 
@@ -145,7 +146,17 @@ reset_search(struct view *view)
 void
 search_view(struct view *view, enum request request)
 {
-	setup_and_find_next(view, request);
+	const char *prompt = request == REQ_SEARCH ? "/" : "?";
+	char *search = read_prompt(prompt);
+
+	if (search) {
+		string_ncopy(argv_env.search, search, strlen(search));
+		setup_and_find_next(view, request);
+	} else if (*argv_env.search) {
+		find_next(view, request);
+	} else {
+		report_clear();
+	}
 }
 
 /* vim: set ts=8 sw=8 noexpandtab: */

src/prompt.c
@@ -789,7 +789,7 @@ run_prompt_command(struct view *view, const char *argv[])
 			return cmd[0] == '/' ? REQ_FIND_NEXT : REQ_FIND_PREV;
 
 		string_ncopy(view->env->search, search + 1, strlen(search + 1));
-		return cmd[0] == '/' ? REQ_SEARCH : REQ_SEARCH_BACK;
+		return cmd[0] == '/' ? REQ_FIND_NEXT : REQ_FIND_PREV;
 
 	} else if (cmdlen > 1 && cmd[0] == '!') {
 		struct view *next = &pager_view;

src/tig.c
@@ -705,22 +705,6 @@ main(int argc, const char *argv[])
 		case REQ_PROMPT:
 			request = open_prompt(view);
 			break;
-		case REQ_SEARCH:
-		case REQ_SEARCH_BACK:
-		{
-			const char *prompt = request == REQ_SEARCH ? "/" : "?";
-			char *search = read_prompt(prompt);
-
-			if (search)
-				string_ncopy(argv_env.search, search, strlen(search));
-			else if (*argv_env.search)
-				request = request == REQ_SEARCH ?
-					REQ_FIND_NEXT :
-					REQ_FIND_PREV;
-			else
-				request = REQ_NONE;
-			break;
-		}
 		default:
 			break;
 		}

commit 35e3058654483c4853fc300b5dc12d61846a8fc2

Fix search binding regression introduced in 1be1228
From the ticket:

    After 1be1228, when a previous search pattern exists, :/ and :? do
    not set a new search pattern, and thus function like n and N. This
    breaks search bindings since they must use :/ and :? instead of /
    and ? (which still work). For example, the default bind diff @ :/^@@
    does not work correctly following a search for a different pattern.

    To reproduce: Search /foo and then search :/bar (note :/, not /),
    for any distinct foo and bar.

This is fixed by ensuring that the search is reset when
`view->env->search` is updated by the prompt code.

Fixes #476

src/search.c
@@ -120,7 +120,7 @@ find_next_match(struct view *view, enum request request)
 	enum status_code code;
 	int direction;
 
-	if (!*view->grep) {
+	if (!*view->grep || strcmp(view->grep, view->env->search)) {
 		if (!*view->env->search)
 			return success("No previous search");
 		return setup_and_find_next(view, request);