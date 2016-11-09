5263eda3f020e9f4319c2af66bf85a69cc8baca9
David Lin <dtwlin@google.com>
2016-11-08 19:39:53 -0800

    Fix broken diff-highlight on some platforms
    Fix issue with diff-highlight that the env variable is not null terminated
    
    Refs #313
    
    Signed-off-by: David Lin <dtwlin@google.com>

--- src/diff.c
+++ src/diff.c
@@ -48,7 +48,7 @@ diff_init_highlight(struct view *view, struct diff_state *state)
 {
 	if (opt_diff_highlight) {
 		const char *argv[] = { opt_diff_highlight, NULL };
-		char * const env[] = { "GIT_CONFIG=/dev/null" };
+		char * const env[] = { "GIT_CONFIG=/dev/null", NULL };
 		struct io io;
 
 		if (io_exec(&io, IO_RP, view->dir, env, argv, view->io.pipe)) {
