88a92b39efdf31f480ed0e1a4cdb0f278924e2ce
lbonn <bonnans.l@gmail.com>
2016-11-20 13:01:44 +0100

    completion: Simple rewrite attempt
    It re-uses utilities from the classic git completion script (with
    `_completion_loader`)

--- contrib/tig-completion.bash
+++ contrib/tig-completion.bash
@@ -15,262 +15,125 @@
 #
 # To use these routines:
 #
-#    1) Copy this file to somewhere (e.g. ~/.tig-completion.sh).
-#    2) Added the following line to your .bashrc:
+#    1) Install and activate the base git completion
+#    2) Copy this file to somewhere (e.g. ~/.tig-completion.sh).
+#    3) Added the following line to your .bashrc:
 #        source ~/.tig-completion.sh
 #
-#    3) You may want to make sure the git executable is available
+#    4) You may want to make sure the git executable is available
 #       in your PATH before this script is sourced, as some caching
 #       is performed while the script loads.  If git isn't found
 #       at source time then all lookups will be done on demand,
 #       which may be slightly slower.
 #
 
-__tigdir ()
-{
-	if [ -z "$1" ]; then
-		if [ -n "$__git_dir" ]; then
-			echo "$__git_dir"
-		elif [ -d .git ]; then
-			echo .git
-		else
-			git rev-parse --git-dir 2>/dev/null
-		fi
-	elif [ -d "$1/.git" ]; then
-		echo "$1/.git"
-	else
-		echo "$1"
-	fi
-}
+__tig_cmds="blame status show log stash grep"
 
-_tigcomp ()
+__tig_complete_revlist_or_cmd ()
 {
-	local all c s=$'\n' IFS=' '$'\t'$'\n'
-	local cur="${COMP_WORDS[COMP_CWORD]}"
-	if [ $# -gt 2 ]; then
-		cur="$3"
-	fi
-	for c in $1; do
-		case "$c$4" in
-		--*=*) all="$all$c$4$s" ;;
-		*.)    all="$all$c$4$s" ;;
-		*)     all="$all$c$4 $s" ;;
-		esac
-	done
-	IFS=$s
-	COMPREPLY=($(compgen -P "$2" -W "$all" -- "$cur"))
-	return
-}
-
-__tig_refs ()
-{
-	local cmd i is_hash=y dir="$(__tigdir "$1")"
-	if [ -d "$dir" ]; then
-		for i in HEAD FETCH_HEAD ORIG_HEAD MERGE_HEAD; do
-			if [ -e "$dir/$i" ]; then echo $i; fi
-		done
-		for i in $(git --git-dir="$dir" \
-			for-each-ref --format='%(refname)' \
-			refs/tags refs/heads refs/remotes); do
-			case "$i" in
-				refs/tags/*)    echo "${i#refs/tags/}" ;;
-				refs/heads/*)   echo "${i#refs/heads/}" ;;
-				refs/remotes/*) echo "${i#refs/remotes/}" ;;
-				*)              echo "$i" ;;
-			esac
-		done
+	local pfx ls ref cur_="$cur"
+	case "$cur_" in
+	*..?*:*)
 		return
-	fi
-	for i in $(git-ls-remote "$dir" 2>/dev/null); do
-		case "$is_hash,$i" in
-		y,*) is_hash=n ;;
-		n,*^{}) is_hash=y ;;
-		n,refs/tags/*) is_hash=y; echo "${i#refs/tags/}" ;;
-		n,refs/heads/*) is_hash=y; echo "${i#refs/heads/}" ;;
-		n,refs/remotes/*) is_hash=y; echo "${i#refs/remotes/}" ;;
-		n,*) is_hash=y; echo "$i" ;;
-		esac
-	done
-}
-
-__tig_complete_file ()
-{
-	local pfx ls ref cur="${COMP_WORDS[COMP_CWORD]}"
-	case "$cur" in
+		;;
 	?*:*)
-		ref="${cur%%:*}"
-		cur="${cur#*:}"
-		case "$cur" in
+		ref="${cur_%%:*}"
+		cur_="${cur_#*:}"
+		case "$cur_" in
 		?*/*)
-			pfx="${cur%/*}"
-			cur="${cur##*/}"
+			pfx="${cur_%/*}"
+			cur_="${cur_##*/}"
 			ls="$ref:$pfx"
 			pfx="$pfx/"
 			;;
 		*)
 			ls="$ref"
 			;;
-	    esac
-		COMPREPLY=($(compgen -P "$pfx" \
-			-W "$(git --git-dir="$(__tigdir)" ls-tree "$ls" \
-				| sed '/^100... blob /s,^.*	,,
+		esac
+
+		case "$COMP_WORDBREAKS" in
+		*:*) : great ;;
+		*)   pfx="$ref:$pfx" ;;
+		esac
+
+		__gitcomp_nl "$(git --git-dir="$(__gitdir)" ls-tree "$ls" 2>/dev/null \
+				| sed '/^100... blob /{
+				           s,^.*	,,
+				           s,$, ,
+				       }
+				       /^120000 blob /{
+				           s,^.*	,,
+				           s,$, ,
+				       }
 				       /^040000 tree /{
 				           s,^.*	,,
 				           s,$,/,
 				       }
 				       s/^.*	//')" \
-			-- "$cur"))
+			"$pfx" "$cur_" ""
 		;;
-	*)
-		_tigcomp "$(__tig_refs)"
-		;;
-	esac
-}
-
-__tig_complete_revlist ()
-{
-	local pfx cur="${COMP_WORDS[COMP_CWORD]}"
-	case "$cur" in
 	*...*)
-		pfx="${cur%...*}..."
-		cur="${cur#*...}"
-		_tigcomp "$(__tig_refs)" "$pfx" "$cur"
+		pfx="${cur_%...*}..."
+		cur_="${cur_#*...}"
+		__gitcomp_nl "$(__git_refs)" "$pfx" "$cur_"
 		;;
 	*..*)
-		pfx="${cur%..*}.."
-		cur="${cur#*..}"
-		_tigcomp "$(__tig_refs)" "$pfx" "$cur"
-		;;
-	*.)
-		_tigcomp "$cur."
+		pfx="${cur_%..*}.."
+		cur_="${cur_#*..}"
+		__gitcomp_nl "$(__git_refs)" "$pfx" "$cur_"
 		;;
 	*)
-		_tigcomp "$(__tig_refs)"
+		__gitcomp "$__tig_cmds"
+		__gitcomp_nl_append "$(__git_refs)"
 		;;
 	esac
-}
-
-_tig_options ()
-{
-	local cur="${COMP_WORDS[COMP_CWORD]}"
-	case "$cur" in
-	--pretty=*)
-		_tigcomp "
-			oneline short medium full fuller email raw
-			" "" "${cur##--pretty=}"
-		return
-		;;
-	--*)
-		_tigcomp "
-			--max-count= --max-age= --since= --after=
-			--min-age= --before= --until=
-			--root --not --topo-order --date-order
-			--no-merges
-			--abbrev-commit --abbrev=
-			--relative-date
-			--author= --committer= --grep=
-			--all-match
-			--pretty= --name-status --name-only
-			--not --all
-			--help --version
-			"
-		return
-		;;
-	-*)
-		_tigcomp "-v -h"
-		return
-		;;
-	esac
-	__tig_complete_revlist
-}
-
-_tig_blame ()
-{
-	local reply="" ref=HEAD cur="${COMP_WORDS[COMP_CWORD]}" p=""
-	local pfx=$(git rev-parse --show-prefix 2>/dev/null)
-
-	if test "$COMP_CWORD" -lt 3; then
-		_tigcomp "$(__tig_refs)"
-	else
-		ref="${COMP_WORDS[2]}"
-	fi
-
-	case "$cur" in
-		*/)	p=${cur%/} ;;
-		*/*)	p=${cur%/*} ;;
-		*)	p= ;;
-	esac
-
-	i=${#COMPREPLY[@]}
-	local IFS=$'\n'
-	for c in $(git --git-dir="$(__tigdir)" ls-tree "$ref:$pfx$p" 2>/dev/null |
-		sed -n '/^100... blob /{
-			s,^.*	,,
-			s,$, ,
-			p
-		}
-		/^040000 tree /{
-			s,^.*	,,
-			s,$,/,
-			p
-		}')
-	do
-		c="${p:+$p/}$c"
-		if [[ "$c" == "$cur"* ]]; then
-			COMPREPLY[i++]=$c
-		fi
-	done
-}
-
-_tig_show ()
-{
-	local cur="${COMP_WORDS[COMP_CWORD]}"
-	case "$cur" in
-	--pretty=*)
-		_tigcomp "
-			oneline short medium full fuller email raw
-			" "" "${cur##--pretty=}"
-		return
-		;;
-	--*)
-		_tigcomp "--pretty="
-		return
-		;;
-	esac
-	__tig_complete_file
 }
 
 _tig ()
 {
-	local i c=1 command __tig_dir
+	local cur words cword prev
+	local i c=1 command
 
-	while [ $c -lt $COMP_CWORD ]; do
-		i="${COMP_WORDS[c]}"
+	# load all completion utilities from git
+	_completion_loader git
+
+	_get_comp_words_by_ref -n =: cur words cword prev
+
+	while [ $c -lt $cword ]; do
+		i="${words[c]}"
 		case "$i" in
 		--) command="log"; break;;
 		-*) ;;
 		*) command="$i"; break ;;
 		esac
-		c=$((++c))
+		((c++))
 	done
 
-	if [ $c -eq $COMP_CWORD -a -z "$command" ]; then
-		case "${COMP_WORDS[COMP_CWORD]}" in
-		--*=*) COMPREPLY=() ;;
-		-*)   _tig_options ;;
-		*)    _tigcomp "blame status show log stash grep $(__tig_refs)" ;;
+	if [ $c -eq $cword -a -z "$command" ]; then
+		case "$cur" in
+		--*)
+			_git_log
+			;;
+		-*)
+			__gitcomp "-v -h"
+			;;
+		*)
+			__tig_complete_revlist_or_cmd ;;
 		esac
 		return
 	fi
 
 	case "$command" in
-	blame)  _tig_blame ;;
-	show)   _tig_show ;;
-	status) ;;
-	*)	_tigcomp "
-			$(__tig_complete_file)
-			$(__tig_refs)
-		" ;;
+	show)
+		_git_show ;;
+	log)
+		_git_log ;;
+	status)
+		_git_status;;
+	blame)
+		__gitcomp "$(__git_complete_file)";;
+	*)
+		_git_log ;;
 	esac
 }
 
