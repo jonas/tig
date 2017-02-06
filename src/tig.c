2c2c2796407800a9f3eceba5a5e70d9e574afaff
Jonas Fonseca <jonas.fonseca@gmail.com>
2017-02-05 23:32:51 -0500

    Fix parsing of revision arguments
    Use argv_parse_rev_flag to pre-filter arguments into opt_rev_arg since
    passing them through git-rev-parse(1) will cause certain flags like
    `--exclude=...` to be silently dropped.
    
    Fixes #555

--- src/tig.c
+++ src/tig.c
@@ -408,6 +408,23 @@ filter_options(const char *argv[], bool rev_parse)
 		return;
 	}
 
+	/* Add known revision arguments in opt_rev_args and use
+	 * git-rev-parse to filter out the remaining options.
+	 */
+	for (next = flags_pos = 0; argv[next]; next++) {
+		const char *arg = argv[next];
+
+		if (!strcmp(arg, "--"))
+			while (!argv[next])
+				argv[flags_pos++] = argv[next++];
+		else if (argv_parse_rev_flag(arg, NULL))
+			argv_append(&opt_rev_args, arg);
+		else
+			argv[flags_pos++] = arg;
+	}
+
+	argv[flags_pos] = NULL;
+
 	filter_rev_parse(&opt_file_args, "--no-revs", "--no-flags", argv);
 	filter_rev_parse(&flags, "--flags", "--no-revs", argv);
 
@@ -425,17 +442,6 @@ filter_options(const char *argv[], bool rev_parse)
 
 		opt_cmdline_args = flags;
 	}
-
-	for (next = flags_pos = 0; argv[next]; next++) {
-		const char *arg = argv[next];
-
-		if (!strcmp(arg, "--all"))
-			argv_append(&opt_rev_args, arg);
-		else
-			argv[flags_pos++] = arg;
-	}
-
-	argv[flags_pos] = NULL;
 
 	filter_rev_parse(&opt_rev_args, "--symbolic", "--revs-only", argv);
 }
