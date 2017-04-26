fed1bd123ce25857b9ad2e08b6a46d8436a2b62d
Delyan Angelov <delian66@gmail.com>
2017-04-26 22:53:43 +0300

    Fix refresh-mode = periodic .

--- src/display.c
+++ src/display.c
@@ -574,8 +574,10 @@ get_input(int prompt_position, struct key *key)
 			delay = watch_periodic(opt_refresh_interval);
 			foreach_displayed_view (view, i) {
 				if (view_can_refresh(view) &&
-				    watch_dirty(&view->watch))
+				    watch_dirty(&view->watch)){
+					load_refs(true);
 					refresh_view(view);
+				}
 			}
 		}
 
