jonas/tig

commit 39acb5ab03a6e3b02eaeca6c9387301615e1524e

Setup the LINES/COLUMNS env vars before loading a view
Previously the env vars were only updated once the view was loaded,
which caused diff stats to not obey the view width. This also ensures
that the env vars are correct for the initial view.

src/view.c
@@ -567,13 +567,42 @@ view_no_refresh(struct view *view, enum open_flags flags)
 }
 
 bool
+view_exec(struct view *view, enum open_flags flags)
+{
+	char opt_env_lines[64] = "";
+	char opt_env_columns[64] = "";
+	char * const opt_env[]	= { opt_env_lines, opt_env_columns, NULL };
+
+	enum io_flags forward_stdin = (flags & OPEN_FORWARD_STDIN) ? IO_RD_FORWARD_STDIN : 0;
+	enum io_flags with_stderr = (flags & OPEN_WITH_STDERR) ? IO_RD_WITH_STDERR : 0;
+	enum io_flags io_flags = forward_stdin | with_stderr;
+
+	int views = displayed_views();
+	bool split = (views == 1 && !!(flags & OPEN_SPLIT)) || views == 2;
+	int height, width;
+
+	getmaxyx(stdscr, height, width);
+	if (split && vertical_split_is_enabled(opt_vertical_split, height, width)) {
+		bool is_base_view = display[0] == view;
+		int split_width = apply_vertical_split(width);
+
+		if (is_base_view)
+			width -= split_width;
+		else
+			width = split_width - 1;
+	}
+
+	string_format(opt_env_columns, "COLUMNS=%d", MAX(0, width));
+	string_format(opt_env_lines, "LINES=%d", height);
+
+	return io_exec(&view->io, IO_RD, view->dir, opt_env, view->argv, io_flags);
+}
+
+bool
 begin_update(struct view *view, const char *dir, const char **argv, enum open_flags flags)
 {
 	bool extra = !!(flags & (OPEN_EXTRA));
 	bool refresh = flags & (OPEN_REFRESH | OPEN_PREPARED | OPEN_STDIN);
-	enum io_flags forward_stdin = (flags & OPEN_FORWARD_STDIN) ? IO_RD_FORWARD_STDIN : 0;
-	enum io_flags with_stderr = (flags & OPEN_WITH_STDERR) ? IO_RD_WITH_STDERR : 0;
-	enum io_flags io_flags = forward_stdin | with_stderr;
 
 	if (view_no_refresh(view, flags))
 		return true;
@@ -598,7 +627,7 @@ begin_update(struct view *view, const char *dir, const char **argv, enum open_fl
 	}
 
 	if (view->argv && view->argv[0] &&
-	    !io_exec(&view->io, IO_RD, view->dir, opt_env, view->argv, io_flags)) {
+	    !view_exec(view, flags)) {
 		report("Failed to open %s view", view->name);
 		return false;
 	}
@@ -750,6 +779,9 @@ split_view(struct view *prev, struct view *view)
 		/* "Blur" the previous view. */
 		update_view_title(prev);
 	}
+
+	if (view_has_flags(prev, VIEW_FLEX_WIDTH))
+		load_view(prev, NULL, OPEN_RELOAD);
 }
 
 void
@@ -763,6 +795,9 @@ maximize_view(struct view *view, bool redraw)
 		redraw_display(false);
 		report_clear();
 	}
+
+	if (view_has_flags(view, VIEW_FLEX_WIDTH))
+		load_view(view, NULL, OPEN_RELOAD);
 }
 
 void

include/tig/display.h
@@ -47,6 +47,9 @@ void resize_display(void);
 void redraw_display(bool clear);
 bool save_display(const char *path);
 
+bool vertical_split_is_enabled(enum vertical_split vsplit, int height, int width);
+int apply_vertical_split(int base_width);
+
 bool open_external_viewer(const char *argv[], const char *dir, bool silent, bool confirm, bool refresh, const char *notice);
 void open_editor(const char *file, unsigned int lineno);
 void enable_mouse(bool enable);

include/tig/options.h
@@ -164,9 +164,6 @@ union view_column_options {
 
 extern iconv_t opt_iconv_out;
 extern char opt_editor[SIZEOF_STR];
-extern char opt_env_lines[64];
-extern char opt_env_columns[64];
-extern char *opt_env[];
 
 /*
  * Mapping between options and command argument mapping.

include/tig/view.h
@@ -58,6 +58,7 @@ enum view_flag {
 	VIEW_REFRESH		= 1 << 13,
 	VIEW_GREP_LIKE		= 1 << 14,
 	VIEW_SORTABLE		= 1 << 15,
+	VIEW_FLEX_WIDTH		= 1 << 16,
 
 	VIEW_RESET_DISPLAY	= 1 << 31,
 };
@@ -286,6 +287,7 @@ void load_view(struct view *view, struct view *prev, enum open_flags flags);
 
 void open_view(struct view *prev, struct view *view, enum open_flags flags);
 void open_argv(struct view *prev, struct view *view, const char *argv[], const char *dir, enum open_flags flags);
+bool view_exec(struct view *view, enum open_flags flags);
 
 /*
  * Various utilities.

src/blame.c
@@ -349,7 +349,7 @@ setup_blame_parent_line(struct view *view, struct blame *blame)
 
 	if (!string_format(from, "%s:%s", view->env->ref, view->env->file) ||
 	    !string_format(to, "%s:%s", blame->commit->id, blame->commit->filename) ||
-	    !io_run(&io, IO_RD, NULL, opt_env, diff_tree_argv))
+	    !io_run(&io, IO_RD, NULL, NULL, diff_tree_argv))
 		return;
 
 	while (io_get(&io, &buf, '\n', true)) {

src/diff.c
@@ -270,7 +270,7 @@ diff_read(struct view *view, struct buffer *buf)
 
 				if (view->pipe)
 					io_done(view->pipe);
-				if (io_run(&view->io, IO_RD, view->dir, opt_env, view->argv))
+				if (view_exec(view, 0))
 					return false;
 			}
 		}
@@ -311,7 +311,7 @@ diff_blame_line(const char *ref, const char *file, unsigned long lineno,
 	if (!string_format(line_arg, "-L%ld,+1", lineno))
 		return false;
 
-	if (!io_run(&io, IO_RD, repo.cdup, opt_env, blame_argv))
+	if (!io_run(&io, IO_RD, repo.cdup, NULL, blame_argv))
 		return false;
 
 	while (io_get(&io, &buf, '\n', true)) {
@@ -551,7 +551,7 @@ diff_select(struct view *view, struct line *line)
 static struct view_ops diff_ops = {
 	"line",
 	argv_env.commit,
-	VIEW_DIFF_LIKE | VIEW_ADD_DESCRIBE_REF | VIEW_ADD_PAGER_REFS | VIEW_FILE_FILTER | VIEW_REFRESH,
+	VIEW_DIFF_LIKE | VIEW_ADD_DESCRIBE_REF | VIEW_ADD_PAGER_REFS | VIEW_FILE_FILTER | VIEW_REFRESH | VIEW_FLEX_WIDTH,
 	sizeof(struct diff_state),
 	diff_open,
 	diff_read,

src/display.c
@@ -135,33 +135,29 @@ apply_horizontal_split(struct view *base, struct view *view)
 	base->height -= view->height;
 }
 
-static void
-apply_vertical_split(struct view *base, struct view *view)
+int
+apply_vertical_split(int base_width)
 {
-	view->height = base->height;
-	view->width  = apply_step(opt_split_view_width, base->width);
-	view->width  = MAX(view->width, MIN_VIEW_WIDTH);
-	view->width  = MIN(view->width, base->width - MIN_VIEW_WIDTH);
-	base->width -= view->width;
+	int width  = apply_step(opt_split_view_width, base_width);
+
+	width = MAX(width, MIN_VIEW_WIDTH);
+	width = MIN(width, base_width - MIN_VIEW_WIDTH);
+
+	return width;
 }
 
-static bool
-vertical_split_is_enabled(void)
+bool
+vertical_split_is_enabled(enum vertical_split vsplit, int height, int width)
 {
-	if (opt_vertical_split == VERTICAL_SPLIT_AUTO) {
-		int height, width;
-
-		getmaxyx(stdscr, height, width);
+	if (vsplit == VERTICAL_SPLIT_AUTO)
 		return width > 160 || width * VSPLIT_SCALE > (height - 1) * 2;
-	}
-
-	return opt_vertical_split == VERTICAL_SPLIT_VERTICAL;
+	return vsplit == VERTICAL_SPLIT_VERTICAL;
 }
 
 static void
 redraw_display_separator(bool clear)
 {
-	if (displayed_views() > 1 && vertical_split_is_enabled()) {
+	if (display_sep) {
 		chtype separator = opt_line_graphics ? ACS_VLINE : '|';
 
 		if (clear)
@@ -175,35 +171,52 @@ void
 resize_display(void)
 {
 	int x, y, i;
+	int height, width;
 	struct view *base = display[0];
 	struct view *view = display[1] ? display[1] : display[0];
 	bool vsplit;
 
 	/* Setup window dimensions */
 
-	getmaxyx(stdscr, base->height, base->width);
+	getmaxyx(stdscr, height, width);
 
 	/* Make room for the status window. */
-	base->height -= 1;
+	base->height = height - 1;
+	base->width = width;
 
-	vsplit = vertical_split_is_enabled();
+	vsplit = vertical_split_is_enabled(opt_vertical_split, height, width);
 
 	if (view != base) {
 		if (vsplit) {
-			apply_vertical_split(base, view);
+			view->height = base->height;
+			view->width = apply_vertical_split(base->width);
+			base->width -= view->width;
 
 			/* Make room for the separator bar. */
 			view->width -= 1;
+
+			if (!display_sep) {
+				display_sep = newwin(base->height, 1, 0, base->width);
+				if (!display_sep)
+					die("Failed to create separator window");
+
+			} else {
+				wresize(display_sep, base->height, 1);
+				mvwin(display_sep, 0, base->width);
+			}
+
+			redraw_display_separator(false);
 		} else {
 			apply_horizontal_split(base, view);
 		}
 
 		/* Make room for the title bar. */
 		view->height -= 1;
-	}
 
-	string_format(opt_env_columns, "COLUMNS=%d", base->width);
-	string_format(opt_env_lines, "LINES=%d", base->height);
+	} else if (display_sep) {
+		delwin(display_sep);
+		display_sep = NULL;
+	}
 
 	/* Make room for the title bar. */
 	base->height -= 1;
@@ -227,18 +240,6 @@ resize_display(void)
 			mvwin(display_win[i], y, x);
 			wresize(display_title[i], 1, view->width);
 			mvwin(display_title[i], y + view->height, x);
-		}
-
-		if (i > 0 && vsplit) {
-			if (!display_sep) {
-				display_sep = newwin(view->height, 1, 0, x - 1);
-				if (!display_sep)
-					die("Failed to create separator window");
-
-			} else {
-				wresize(display_sep, view->height, 1);
-				mvwin(display_sep, 0, x - 1);
-			}
 		}
 
 		view->win = display_win[i];

src/log.c
@@ -140,7 +140,7 @@ log_read(struct view *view, struct buffer *buf)
 static struct view_ops log_ops = {
 	"line",
 	argv_env.head,
-	VIEW_ADD_PAGER_REFS | VIEW_OPEN_DIFF | VIEW_SEND_CHILD_ENTER | VIEW_LOG_LIKE | VIEW_REFRESH,
+	VIEW_ADD_PAGER_REFS | VIEW_OPEN_DIFF | VIEW_SEND_CHILD_ENTER | VIEW_LOG_LIKE | VIEW_REFRESH | VIEW_FLEX_WIDTH,
 	sizeof(struct log_state),
 	log_open,
 	log_read,

commit dad0cf35418d12ce8c2cf33c1fa6d19ea160d8b8

Fix reading from stdin for "tig show"
The regression was introduced in 39acb5ab03a6e3b02eaeca6c9387301615e1524e when
load_view was called unconditionally for diff views causing reading from stdin
to be stopped via a reload.

src/view.c
@@ -726,10 +726,10 @@ maximize_view(struct view *view, bool redraw)
 	if (redraw) {
 		redraw_display(false);
 		report_clear();
-	}
 
-	if (view_has_flags(view, VIEW_FLEX_WIDTH))
-		load_view(view, NULL, OPEN_RELOAD);
+		if (view_has_flags(view, VIEW_FLEX_WIDTH))
+			load_view(view, NULL, OPEN_RELOAD);
+	}
 }
 
 void

test/diff/diff-stdin-test
@@ -1,0 +1,84 @@
+#!/bin/sh
+
+. libtest.sh
+. libgit.sh
+
+steps '
+	:save-display diff.screen
+'
+
+git_init
+
+test_tig show <<EOF
+commit 1b4c64b595aeb4de1d317d669faacd3c1d82f0b0
+Author: Sven Wegener <sven.wegener@example.net>
+Date:   Sun Jul 2 15:01:24 2017 +0200
+
+    graph: Optimize initial and merge calculation
+    
+    The initial and merge flags do not depend on any column-specific
+    information, so calculate them only once.
+    
+    Signed-off-by: Sven Wegener <sven.wegener@example.net>
+---
+ src/graph-v2.c | 7 +++++--
+ 1 file changed, 5 insertions(+), 2 deletions(-)
+
+diff --git a/src/graph-v2.c b/src/graph-v2.c
+index 4cf3d9e..ced6838 100644
+--- a/src/graph-v2.c
++++ b/src/graph-v2.c
+@@ -683,6 +683,9 @@ graph_generate_symbols(struct graph_v2 *graph, struct graph_canvas *canvas)
+ 	struct graph_row *row = &graph->row;
+ 	struct graph_row *next_row = &graph->next_row;
+ 	struct graph_row *parents = &graph->parents;
++	int commits = commits_in_row(parents);
++	int initial = commits < 1;
++	int merge = commits > 1;
+ 	int pos;
+ 
+ 	for (pos = 0; pos < row->size; pos++) {
+@@ -692,8 +695,8 @@ graph_generate_symbols(struct graph_v2 *graph, struct graph_canvas *canvas)
+ 
+ 		symbol->commit            = (pos == graph->position);
+ 		symbol->boundary          = (pos == graph->position && next_row->columns[pos].symbol.boundary);
+-		symbol->initial           = (commits_in_row(parents) < 1);
+-		symbol->merge             = (commits_in_row(parents) > 1);
++		symbol->initial           = initial;
++		symbol->merge             = merge;
+ 
+ 		symbol->continued_down    = continued_down(row, next_row, pos);
+ 		symbol->continued_up      = continued_down(prev_row, row, pos);
+EOF
+
+assert_equals 'diff.screen' <<EOF
+commit 1b4c64b595aeb4de1d317d669faacd3c1d82f0b0
+Author: Sven Wegener <sven.wegener@example.net>
+Date:   Sun Jul 2 15:01:24 2017 +0200
+ 
+    graph: Optimize initial and merge calculation
+ 
+    The initial and merge flags do not depend on any column-specific
+    information, so calculate them only once.
+ 
+    Signed-off-by: Sven Wegener <sven.wegener@example.net>
+---
+ src/graph-v2.c | 7 +++++--
+ 1 file changed, 5 insertions(+), 2 deletions(-)
+ 
+diff --git a/src/graph-v2.c b/src/graph-v2.c
+index 4cf3d9e..ced6838 100644
+--- a/src/graph-v2.c
++++ b/src/graph-v2.c
+@@ -683,6 +683,9 @@ graph_generate_symbols(struct graph_v2 *graph, struct graph_
+        struct graph_row *row = &graph->row;
+        struct graph_row *next_row = &graph->next_row;
+        struct graph_row *parents = &graph->parents;
++       int commits = commits_in_row(parents);
++       int initial = commits < 1;
++       int merge = commits > 1;
+        int pos;
+ 
+        for (pos = 0; pos < row->size; pos++) {
+[diff] 1b4c64b595aeb4de1d317d669faacd3c1d82f0b0 - line 1 of 39               71%
+EOF