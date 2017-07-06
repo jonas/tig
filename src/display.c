jonas/tig

commit 2b384f69b45d00a9310d276fad2dfbfc0aabcc67

Fix clearing of the status window
Leaving a prompt with Ctrl-C was leaving artifacts such as ":^" on
the screen. Fix report_clear to pass an empty string and use that
to detect whether to call wclear.

Possible fix for #623.

src/display.c
@@ -373,13 +373,17 @@ update_status_window(struct view *view, const char *msg, va_list args)
 		return false;
 
 	if (!status_empty || *msg) {
+		bool clear = view && view->has_scrolled && use_scroll_status_wclear;
+
 		wmove(status_win, 0, 0);
-		if (view && view->has_scrolled && use_scroll_status_wclear)
+		if (clear)
 			wclear(status_win);
 		if (*msg) {
 			vwprintw(status_win, msg, args);
 			status_empty = false;
 		} else {
+			if (!clear && !status_empty)
+				wclear(status_win);
 			status_empty = true;
 		}
 		wclrtoeol(status_win);

include/tig/display.h
@@ -24,7 +24,7 @@ extern WINDOW *status_win;
 
 void update_status(const char *msg, ...);
 void report(const char *msg, ...) PRINTF_LIKE(1, 2);
-#define report_clear() report("%s", "")
+#define report_clear() report("")
 
 /*
  * Display management.

commit 2db555914fabaf53b7d941f7ccc4e3fdaaa50289

report_clear() efficiency plus clear after grep
2b384f69b introduced a performance regression due to use of wclear(),
which dirties the entire screen.  The core issue being addressed in
2b384f69b is that readline can leave an incorrect cursor position in
virtual window newscr.

So, explicitly reset the newscr cursor position post-readline, which
enables switching to the much more efficient werase() for erasing the
status area.

Incidentally fix the missed case of report_clear() after a grep prompt.

src/display.c
@@ -430,9 +430,8 @@ report_clear(void)
 		return;
 
 	if (!input_mode && !status_empty) {
-		wclear(status_win);
-		wclrtoeol(status_win);
-		wnoutrefresh(status_win);
+		werase(status_win);
+		doupdate();
 	}
 	status_empty = true;
 	update_view_title(view);

src/grep.c
@@ -99,6 +99,8 @@ grep_prompt(void)
 	int argc = 0;
 	char *grep = read_prompt("grep: ");
 
+	report_clear();
+
 	if (!grep || !argv_from_string_no_quotes(argv, &argc, grep))
 		return false;
 	if (grep_argv)

src/prompt.c
@@ -474,6 +474,9 @@ read_prompt(const char *prompt)
 	if (signal(SIGINT, SIG_DFL) == SIG_ERR)
 		die("Failed to remove sigint handler");
 
+	/* readline can leave the virtual cursor out-of-place */
+	set_cursor_pos(0, 0);
+
 	if (line && !*line) {
 		free(line);
 		line = NULL;