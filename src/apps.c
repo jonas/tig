1277f7f473b6b45edb63e9ab58fecc74e7b1eecf
Roland Walker <walker@pobox.com>
2017-08-05 17:59:32 -0400

    stub apps.c for external applications

---
+++ src/apps.c
@@ -0,0 +1,18 @@
+/* Copyright (c) 2006-2017 Jonas Fonseca <jonas.fonseca@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "tig/tig.h"
+#include "tig/io.h"
+#include "tig/apps.h"
+
+/* vim: set ts=8 sw=8 noexpandtab: */
