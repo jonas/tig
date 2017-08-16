655fff0039426188fcb5f831742a70ec27555e19
Roland Walker <walker@pobox.com>
2017-08-16 08:45:25 -0400

    bug: branch on content in expected file
    if the user didn't specify content for STDERR, that is the same as
    specifying it must be empty

--- test/tools/libtest.sh
+++ test/tools/libtest.sh
@@ -662,7 +662,7 @@ run_test_cases()
 		fi
 
 		assert_equals "$name.screen" < "$name.expected"
-		if [ -s "$name.stderr" ]; then
+		if [ -s "$name-assert-stderr" ]; then
 			assert_equals "$name.stderr" < "$name-assert-stderr"
 		else
 			assert_equals "$name.stderr" < /dev/null
