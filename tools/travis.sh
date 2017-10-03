c95115cb06fde0b53210c81c32ef663a4a732632
Stephen <stephengroat@users.noreply.github.com>
2017-10-03 10:53:25 -0700

    parallelize build using travis
    include brew osx builds and move script to tools dir

---
+++ tools/travis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+build_config () {
+	cp contrib/config.make .
+	make all-debug
+	make update-docs && git diff --exit-code
+	make test
+	make test TEST_OPTS=valgrind
+	if [ $CC = clang ]; then make test-address-sanitizer; fi
+	make DESTDIR=/tmp/bare-destdir install install-doc
+	make DESTDIR=/tmp/bare-destdir uninstall
+	test ! -d /tmp/bare-destdir
+	make prefix=/tmp/bare-prefix install install-doc
+	make prefix=/tmp/bare-prefix uninstall
+	test ! -d /tmp/bare-prefix
+ 	make distclean
+}
+
+build_autoconf () {
+	make dist
+	./configure --prefix=/tmp/conf-prefix
+	make V=1 TEST_SHELL=bash all test install install-doc
+	make uninstall
+	test ! -d /tmp/conf-prefix
+	make clean
+}
+
+if [[ $TRAVIS_TIG = config ]]; then
+	build_config
+elif [[ $TRAVIS_TIG = autoconf ]]; then
+	build_autoconf
+fi
