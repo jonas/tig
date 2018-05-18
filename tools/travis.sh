bfd1927955b12c9908ad108aa1748c1480d339f5
Jonas Fonseca <jonas.fonseca@gmail.com>
2018-05-18 18:16:22 -0400

    Test address sanitizer in a separate build

--- tools/travis.sh
+++ tools/travis.sh
@@ -3,35 +3,50 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-build_config () {
+build_config_make() {
 	cp contrib/config.make .
 	make all-debug
 	make update-docs && git diff --exit-code
 	make test
 	make test TEST_OPTS=valgrind
-	if [ $CC = clang ]; then make test-address-sanitizer; fi
-	make DESTDIR=/tmp/bare-destdir install install-doc
-	make DESTDIR=/tmp/bare-destdir uninstall
-	test ! -d /tmp/bare-destdir
+
 	make prefix=/tmp/bare-prefix install install-doc
 	/tmp/bare-prefix/bin/tig --version
 	make prefix=/tmp/bare-prefix uninstall
 	test ! -d /tmp/bare-prefix
+
  	make distclean
 }
 
-build_autoconf () {
+build_autoconf() {
 	make dist
 	./configure --prefix=/tmp/conf-prefix
-	make V=1 TEST_SHELL=bash all test install install-doc
+	make V=1 TEST_SHELL=bash all test
+
+	make install install-doc
 	/tmp/conf-prefix/bin/tig --version
 	make uninstall
 	test ! -d /tmp/conf-prefix
+
+	make DESTDIR=/tmp/bare-destdir install install-doc
+	/tmp/bare-destdir/tmp/conf-prefix/bin/tig --version
+	make DESTDIR=/tmp/bare-destdir uninstall
+	test ! -d /tmp/bare-destdir
+
 	make clean
 }
 
-if [[ $TRAVIS_TIG = config ]]; then
-	build_config
-elif [[ $TRAVIS_TIG = autoconf ]]; then
-	build_autoconf
-fi
+build_address_sanitizer() {
+	cp contrib/config.make .
+	make test-address-sanitizer
+}
+
+case "$TIG_BUILD" in
+	config.make)		build_config_make ;;
+	autoconf)		build_autoconf ;;
+	address-sanitizer)	build_address_sanitizer ;;
+
+	*)
+		echo "Unknown config: $TIG_BUILD"
+		exit 1
+esac
