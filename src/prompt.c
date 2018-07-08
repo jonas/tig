7872d8a51f427f1b3dfcf94e69fdc2f50042f079
Jonas Fonseca <jonas.fonseca@gmail.com>
2018-07-08 02:46:49 +0200

    Fix issues reported by the clang scan-build tool

--- src/prompt.c
+++ src/prompt.c
@@ -1065,7 +1065,6 @@ run_prompt_command(struct view *view, const char *argv[])
 			prompt_update_display(flags);
 
 		} else {
-			request = view_can_refresh(view) ? REQ_REFRESH : REQ_SCREEN_REDRAW;
 			if (!strcmp(cmd, "color"))
 				init_colors();
 			resize_display();
