jonas/tig

commit dd96223265f5bb10af168ffb7ed7b91af9238a43

Fix pager view not moving up when child view is open

src/pager.c
@@ -155,7 +155,8 @@ pager_request(struct view *view, enum request request, struct line *line)
 	/* Always scroll the view even if it was split. That way
 	 * you can use Enter to scroll through the log view and
 	 * split open each commit diff. */
-	scroll_view(view, REQ_SCROLL_LINE_DOWN);
+	if (view == display[current_view])
+		scroll_view(view, REQ_SCROLL_LINE_DOWN);
 
 	/* FIXME: A minor workaround. Scrolling the view will call report_clear()
 	 * but if we are scrolling a non-current view this won't properly

commit 9fb0a2f5362a582489754a632856f95094f4097c

Restore lazy navigation
Restore #83 behaviour that was broken by dd96223265f5bb10af168ffb7ed7b91af9238a43.

src/pager.c
@@ -163,7 +163,7 @@ pager_request(struct view *view, enum request request, struct line *line)
 	/* Always scroll the view even if it was split. That way
 	 * you can use Enter to scroll through the log view and
 	 * split open each commit diff. */
-	if (view == display[current_view])
+	if (view == display[current_view] || !opt_focus_child)
 		scroll_view(view, REQ_SCROLL_LINE_DOWN);
 
 	/* FIXME: A minor workaround. Scrolling the view will call report_clear()