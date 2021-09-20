jonas/tig

commit 31b7f74f1f602a4452029b8f30930f9fb007dc01

Pass stash args through (#1030)
As the stash reference does not necessarily match the line number anymore,
take it from the output of git stash list.

Closes #1022

src/stash.c
@@ -20,7 +20,7 @@ static enum status_code
 stash_open(struct view *view, enum open_flags flags)
 {
 	static const char *stash_argv[] = { "git", "stash", "list",
-		encoding_arg, "--no-color", "--pretty=raw", NULL };
+		encoding_arg, "--no-color", "--pretty=raw", "%(revargs)", NULL };
 	struct main_state *state = view->private;
 
 	state->with_graph = false;
@@ -31,8 +31,12 @@ stash_open(struct view *view, enum open_flags flags)
 static void
 stash_select(struct view *view, struct line *line)
 {
+	struct main_state *state = view->private;
+
 	main_select(view, line);
-	string_format(view->env->stash, "stash@{%d}", line->lineno - 1);
+	assert(state->reflogs >= line->lineno);
+	string_ncopy(view->env->stash, state->reflog[line->lineno - 1] + STRING_SIZE("refs/"),
+		     strlen(state->reflog[line->lineno - 1]) - STRING_SIZE("refs/"));
 	string_copy(view->ref, view->env->stash);
 }
 

src/tig.c
@@ -378,7 +378,7 @@ static const char usage_string[] =
 "   or: tig blame  [options] [rev] [--] path\n"
 "   or: tig grep   [options] [pattern]\n"
 "   or: tig refs   [options]\n"
-"   or: tig stash\n"
+"   or: tig stash  [options]\n"
 "   or: tig status\n"
 "   or: tig <      [git command output]\n"
 "\n"

commit fe9384ec7f42b508389914e7db8087633932ce3c

Fix stash view when using tig --all or tig revs
This is a regression from #1030 due to a wrong handling of --all,
--branches, --remotes and revisions arguments in `git stash list`.
Workaround the issue by filtering them from %(revargs).

Fixes #1146

src/stash.c
@@ -21,15 +21,35 @@ static enum status_code
 stash_open(struct view *view, enum open_flags flags)
 {
 	static const char *stash_argv[] = { "git", "stash", "list",
-		encoding_arg, "--no-color", "--pretty=raw", "%(revargs)", NULL };
+		encoding_arg, "--no-color", "--pretty=raw", NULL };
+	const char **argv = NULL;
 	struct main_state *state = view->private;
+	enum status_code code;
 
 	if (!(repo.is_inside_work_tree || *repo.worktree))
 		return error("The stash view requires a working tree");
 
+	/* git stash list only works well with commit limiting options,
+	 * so filter --all, --branches, --remotes and revisions from
+	 * %(revargs). */
+	if (!argv_append_array(&argv, stash_argv))
+		return ERROR_OUT_OF_MEMORY;
+	if (opt_rev_args) {
+		int i;
+		for (i = 0; opt_rev_args[i]; i++) {
+			const char *arg = opt_rev_args[i];
+			if (arg[0] == '-' && strcmp(arg, "--all") &&
+			    strcmp(arg, "--branches") && strcmp(arg, "--remotes"))
+				argv_append(&argv, arg);
+		}
+	}
+
 	state->with_graph = false;
 	watch_register(&view->watch, WATCH_STASH);
-	return begin_update(view, NULL, stash_argv, flags | OPEN_RELOAD);
+	code = begin_update(view, NULL, argv, flags | OPEN_RELOAD);
+	argv_free(argv);
+	free(argv);
+	return code;
 }
 
 static void