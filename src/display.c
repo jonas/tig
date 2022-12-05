jonas/tig

commit 68f1c17b5ec814494872c5c41e5ada18c97771c1

Automatically close empty stage views after refreshing

src/stage.c
@@ -530,6 +530,11 @@ stage_read(struct view *view, char *data)
 	if (stage_line_type == LINE_STAT_UNTRACKED)
 		return pager_common_read(view, data, LINE_DEFAULT, NULL);
 
+	if (!data && !view->lines && view->parent) {
+		maximize_view(view->parent, TRUE);
+		return TRUE;
+	}
+
 	if (data && diff_common_read(view, data, &state->diff))
 		return TRUE;
 

commit d1fef83c81990aba76bde21318a3fa9e14a876be

Fix stage view closing when holding the ] key
This is caused by 68f1c17b5ec814494872c5c41e5ada18c97771c1 closing the
stage view when the command output is empty. This would normally happen
when unstaged/staged contents are removed outside of Tig but also when
the command is interrupted.

Do not close the view when stage_read() is called to flush the output.
This makes 78012db73141b6b42ab818d69907c7eb8293bf25 redundant.

Also, make it work for both plain and split views and make sure previous
view gets refreshed automatically.

Fixes #1245

src/display.c
@@ -777,7 +777,6 @@ get_input(int prompt_position, struct key *key)
 
 			foreach_displayed_view (view, i) {
 				if (view_can_refresh(view) &&
-					!view->pipe &&
 					watch_dirty(&view->watch)) {
 					refresh_view(view);
 				}

src/stage.c
@@ -664,6 +664,11 @@ stage_request(struct view *view, enum request request, struct line *line)
 			view->env->goto_lineno--;
 		return request;
 
+	case REQ_VIEW_CLOSE:
+	case REQ_VIEW_CLOSE_NO_QUIT:
+		stage_line_type = 0;
+		return request;
+
 	case REQ_ENTER:
 		return diff_common_enter(view, request, line);
 
@@ -776,6 +781,9 @@ stage_read(struct view *view, struct buffer *buf, bool force_stop)
 {
 	struct stage_state *state = view->private;
 
+	if (!stage_line_type)
+		return true;
+
 	if (stage_line_type == LINE_STAT_UNTRACKED)
 		return pager_common_read(view, buf ? buf->data : NULL, LINE_DEFAULT, NULL);
 
@@ -786,9 +794,11 @@ stage_read(struct view *view, struct buffer *buf, bool force_stop)
 		}
 	}
 
-	if (!buf && !view->lines && view->parent) {
-		maximize_view(view->parent, true);
-		return true;
+	if (!buf && !view->lines && !force_stop && view->prev) {
+		watch_apply(&view->watch, WATCH_INDEX);
+		stage_line_type = 0;
+		maximize_view(view->prev, false);
+		return false;
 	}
 
 	if (!buf)