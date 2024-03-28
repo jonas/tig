a37ce5cfe1f256b81092dda365b1d66450e0929e
Thomas Koutcher <thomas.koutcher@online.fr>
2024-03-28 19:00:54 +0100

    Fix parsing of `git ls-tree` for submodules
    Closes #1282

--- src/draw.c
+++ src/draw.c
@@ -276,11 +276,10 @@ draw_id(struct view *view, struct view_column *column, const char *id)
 }
 
 static bool
-draw_filename(struct view *view, struct view_column *column, const char *filename, mode_t mode)
+draw_filename(struct view *view, struct view_column *column, enum line_type type, const char *filename)
 {
 	size_t width = filename ? utf8_width(filename) : 0;
 	bool trim = width >= column->width;
-	enum line_type type = S_ISDIR(mode) ? LINE_DIRECTORY : LINE_FILE;
 	int column_width = column->width ? column->width : width;
 
 	if (column->opt.file_name.display == FILENAME_NO)
@@ -290,9 +289,9 @@ draw_filename(struct view *view, struct view_column *column, const char *filenam
 }
 
 static bool
-draw_file_size(struct view *view, struct view_column *column, unsigned long size, mode_t mode)
+draw_file_size(struct view *view, struct view_column *column, enum line_type type, unsigned long size)
 {
-	const char *str = S_ISDIR(mode) ? NULL : mkfilesize(size, column->opt.file_size.display);
+	const char *str = type == LINE_FILE ? mkfilesize(size, column->opt.file_size.display) : NULL;
 
 	if (!column->width || column->opt.file_size.display == FILE_SIZE_NO)
 		return false;
@@ -518,7 +517,7 @@ view_column_draw(struct view *view, struct line *line, unsigned int lineno)
 			continue;
 
 		case VIEW_COLUMN_FILE_SIZE:
-			if (draw_file_size(view, column, column_data.file_size ? *column_data.file_size : 0, mode))
+			if (draw_file_size(view, column, line->type, column_data.file_size ? *column_data.file_size : 0))
 				return true;
 			continue;
 
@@ -529,7 +528,7 @@ view_column_draw(struct view *view, struct line *line, unsigned int lineno)
 			continue;
 
 		case VIEW_COLUMN_FILE_NAME:
-			if (draw_filename(view, column, column_data.file_name, mode))
+			if (draw_filename(view, column, line->type, column_data.file_name))
 				return true;
 			continue;
 
