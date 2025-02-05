jonas/tig

commit 2a8ef0ffbc6c52270c9ce60d16dcf2c544428f92

Open the blob corresponding to the diff line
Fixes #1334

src/diff.c
@@ -633,7 +633,7 @@ diff_get_lineno(struct view *view, struct line *line, bool old)
 }
 
 static enum request
-diff_trace_origin(struct view *view, struct line *line)
+diff_trace_origin(struct view *view, enum request request, struct line *line)
 {
 	struct line *commit_line = find_prev_line_by_type(view, line, LINE_COMMIT);
 	char id[SIZEOF_REV];
@@ -652,16 +652,9 @@ diff_trace_origin(struct view *view, struct line *line)
 		return REQ_NONE;
 	}
 
-	for (; diff < line && !file; diff++) {
-		const char *data = box_text(diff);
+	file = diff_get_pathname(view, line, line->type == LINE_DIFF_DEL);
 
-		if (!prefixcmp(data, "--- a/")) {
-			file = data + STRING_SIZE("--- a/");
-			break;
-		}
-	}
-
-	if (diff == line || !file) {
+	if (!file) {
 		report("Failed to read the file name");
 		return REQ_NONE;
 	}
@@ -704,10 +697,10 @@ diff_trace_origin(struct view *view, struct line *line)
 	}
 
 	string_ncopy(view->env->file, commit.filename, strlen(commit.filename));
-	string_copy(view->env->ref, header.id);
+	string_copy(request == REQ_VIEW_BLAME ? view->env->ref : view->env->commit, header.id);
 	view->env->goto_lineno = header.orig_lineno - 1;
 
-	return REQ_VIEW_BLAME;
+	return request;
 }
 
 const char *
@@ -782,7 +775,8 @@ diff_request(struct view *view, enum request request, struct line *line)
 {
 	switch (request) {
 	case REQ_VIEW_BLAME:
-		return diff_trace_origin(view, line);
+	case REQ_VIEW_BLOB:
+		return diff_trace_origin(view, request, line);
 
 	case REQ_EDIT:
 		return diff_common_edit(view, request, line);

commit e4e63b9dc9ba5c48ee26c06ca168e32c7b5c08be

Fix empty blame view when opened from the diff view on i586
The regression was introduced by 2a8ef0ffbc6c52270c9ce60d16dcf2c544428f92.

Fixes #1362

src/diff.c
@@ -698,7 +698,7 @@ diff_trace_origin(struct view *view, enum request request, struct line *line)
 	}
 
 	string_ncopy(view->env->file, commit.filename, strlen(commit.filename));
-	string_copy(request == REQ_VIEW_BLAME ? view->env->ref : view->env->commit, header.id);
+	string_copy_rev(request == REQ_VIEW_BLAME ? view->env->ref : view->env->commit, header.id);
 	view->env->goto_lineno = header.orig_lineno - 1;
 
 	return request;