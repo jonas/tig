e77e4f6ade588ba3a3c2892c24acf267de4f3f95
Thomas Koutcher <thomas.koutcher@online.fr>
2026-02-21 16:23:00 +0100

    Document %(status)
    Closes #1406

--- src/main.c
+++ src/main.c
@@ -666,6 +666,7 @@ main_select(struct view *view, struct line *line)
 			ref_update_env(view->env, ref, true);
 		else
 			view->env->tag[0] = view->env->remote[0] = view->env->branch[0] = view->env->refname[0] = 0;
+		view->env->status[0] = 0;
 	}
 	string_copy_rev(view->env->commit, commit->id);
 	view->env->blob[0] = 0;
