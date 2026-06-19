jonas/tig

commit bb3bbf215728b5276ebf51a645766bdb37d42a32

Enable direct blob edits in clean HEAD state (#1416)
Closes #1415

[tk: transposed code to blob_open() and added status check]

src/blob.c
@@ -81,10 +81,19 @@ blob_open(struct view *view, enum open_flags flags)
 		const char *rev_parse_argv[] = {
 			"git", "rev-parse", blob_spec, NULL
 		};
+		const char *status_argv[] = {
+			"git", "status", "-s", view->env->file, NULL
+		};
+		char buf[SIZEOF_STR] = "";
+
 
 		if (!string_format(blob_spec, "%s:%s", commit, view->env->file) ||
 		    !io_run_buf(rev_parse_argv, view->env->blob, sizeof(view->env->blob), NULL, false))
 			return error("Failed to resolve blob from file name");
+
+		if (is_head_commit(view->env->commit) && view->env->file[0] &&
+		    !io_run_buf(status_argv, buf, sizeof(buf), NULL, false))
+			state->file = get_path(view->env->file);
 
 		string_ncopy(state->commit, commit, strlen(commit));
 	}

commit 1d81bbbfac967b552ab86e6e8449273c89d299e6

Fix blob edit in bare repository
The regression was introduced by #1416 in bb3bbf215728b5276ebf51a645766bdb37d42a32.

src/blob.c
@@ -79,6 +79,7 @@ blob_open(struct view *view, enum open_flags flags)
 		char buf[SIZEOF_STR] = "";
 
 		if (is_head_commit(view->env->commit) && view->env->file[0] &&
+		    (repo.is_inside_work_tree || *repo.worktree) &&
 		    !io_run_buf(status_argv, buf, sizeof(buf), NULL, false))
 			state->file = get_path(view->env->file);
 